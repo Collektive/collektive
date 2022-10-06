173b7c0afabb672ea661f3342d5f84acd79c79a7
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2022-10-06 21:15:09 +0200

    feat(field): added to string method

--- src/commonMain/kotlin/Field.kt
+++ src/commonMain/kotlin/Field.kt
@@ -2,4 +2,7 @@ interface Field<out T>
 
 class FieldImpl<T> : Field<T> {
     val field: Map<Int, T> = emptyMap()
+    override fun toString(): String {
+        return "FieldImpl(field=$field)"
+    }
 }
