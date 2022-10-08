f05a7b8d016bd09a21bbf7703f468b280091ece1
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2022-10-08 18:48:22 +0200

    test(fields): changed test to match the new fields class

--- src/commonTest/kotlin/FieldsTest.kt
+++ src/commonTest/kotlin/FieldsTest.kt
@@ -1,9 +1,25 @@
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
 
 class FieldsTest {
+    private val fields = Fields<Any>()
     @Test
     fun addField(){
-        assertEquals(Fields.addField(FieldImpl<Double>()), true)
+        fields.addField(Int)
+        assertEquals(1, fields.retrieveAllFields().size)
+    }
+
+    @Test
+    fun addFieldOFDifferentType(){
+        fields.addField(Int)
+        fields.addField(String)
+        assertEquals(2, fields.retrieveAllFields().size)
+    }
+
+    @Test
+    fun retrieveField() {
+        fields.addField(Int)
+        assertNotNull(fields.retrieveField(Int))
     }
 }
