a2543df1ee7657717cd5979b1c02dc443c8e6f28
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2022-10-08 18:46:42 +0200

    feat(main): created main to check the possible result

--- src/commonMain/kotlin/Main.kt
+++ src/commonMain/kotlin/Main.kt
@@ -1,9 +1,18 @@
 fun main() {
-    val field = FieldImpl<Double>()
     println("Running on ${Platform.name}")
+    val fields = Fields<Any>()
+
+    val f: (Int) -> Int = { it * 2 }
+    fields.addField(f)
+    fields.addField(String)
+
+    val field = fields.retrieveField(String)
+    field.addElement(0, "test")
+
+    println(fields.retrieveField(String))
 
     // DSL functions
     aggregate {
-        neighbouring()
+
     }
 }
