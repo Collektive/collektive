dc1d5d01b62aa14baae188fbebdf9e14617ebe25
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2022-10-08 18:15:45 +0200

    test(field): test adding entry to field

--- src/commonTest/kotlin/FieldTest.kt
+++ src/commonTest/kotlin/FieldTest.kt
@@ -1,9 +1,16 @@
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 class FieldTest {
     @Test
     fun emptyField(){
         assertEquals(FieldImpl<String>().field, emptyMap())
+    }
+    @Test
+    fun addElementToField(){
+        val field = FieldImpl<String>()
+        field.addElement(0, "test")
+        assertTrue(field.field.containsValue("test"))
     }
 }
