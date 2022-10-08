923db96fec5862e76e1537c9e726598c93847734
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2022-10-08 18:49:17 +0200

    test(fields): added modify field test

--- src/commonTest/kotlin/FieldsTest.kt
+++ src/commonTest/kotlin/FieldsTest.kt
@@ -1,4 +1,5 @@
 import kotlin.test.Test
+import kotlin.test.assertContains
 import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
 
@@ -21,5 +22,14 @@ class FieldsTest {
     fun retrieveField() {
         fields.addField(Int)
         assertNotNull(fields.retrieveField(Int))
+    }
+
+    @Test
+    fun modifyField() {
+        fields.addField(String)
+        val field = fields.retrieveField(String)
+        field.addElement(0, "test")
+        println(fields.retrieveAllFields())
+        assertContains(fields.retrieveField(String).field, 0)
     }
 }
