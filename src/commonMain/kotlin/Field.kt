ae19be8db470c32d583b5efb55a6cd38d5571803
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2022-10-08 18:13:20 +0200

    fix(field): changed map from immutable to mutable

--- src/commonMain/kotlin/Field.kt
+++ src/commonMain/kotlin/Field.kt
@@ -1,7 +1,8 @@
 interface Field<out T>
 
 class FieldImpl<T> : Field<T> {
-    val field: Map<Int, T> = emptyMap()
+    val field: MutableMap<Int, T> = mutableMapOf()
+
     override fun toString(): String {
         return "FieldImpl(field=$field)"
     }
