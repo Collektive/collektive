ac3d3cbbdb1538e784d76b13495c519e9a7b2e21
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2022-10-08 18:14:11 +0200

    feat(field): added function to add a new entry in field

--- src/commonMain/kotlin/Field.kt
+++ src/commonMain/kotlin/Field.kt
@@ -2,7 +2,7 @@ interface Field<out T>
 
 class FieldImpl<T> : Field<T> {
     val field: MutableMap<Int, T> = mutableMapOf()
-
+     fun addElement(id: Int, value: T): T? = field.put(id, value)
     override fun toString(): String {
         return "FieldImpl(field=$field)"
     }
