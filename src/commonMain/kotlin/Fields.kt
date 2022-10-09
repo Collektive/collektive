535b4914de2f1ec7c46b921a97a1cc3b09e32cbb
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2022-10-09 13:51:45 +0200

    feat(fields): changed map key in the hashCode of the event

--- src/commonMain/kotlin/Fields.kt
+++ src/commonMain/kotlin/Fields.kt
@@ -1,12 +1,14 @@
 class Fields<X : Any> {
-    private val fields: MutableMap<X, FieldImpl<X>> = mutableMapOf()
+    private val fields: MutableMap<Int, FieldImpl<X>> = mutableMapOf()
 
-    fun retrieveAllFields(): List<FieldImpl<X>> = fields.values.toList()
+    fun retrieveAllFields(): List<FieldImpl<X>> {
+        println(fields.toString())
+        return fields.values.toList()
+    }
 
-    fun addField(event: X): FieldImpl<X>? = fields.put(event, FieldImpl())
+    fun addField(event: X): FieldImpl<X>? = fields.put(event::class.hashCode(), FieldImpl())
 
-    fun retrieveField(event: X): FieldImpl<X>? = fields[event]
+    fun retrieveField(event: X): FieldImpl<X>? = fields[event::class.hashCode()]
 
-    fun isFieldPresent(event: X): Boolean = fields.containsKey(event)
-
+    fun isFieldPresent(event: X): Boolean = fields.containsKey(event::class.hashCode())
 }
