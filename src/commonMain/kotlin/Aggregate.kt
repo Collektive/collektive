349694e9ed07a5f35ab00eafc5641ece032e5373
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2022-10-09 11:48:39 +0200

    feat(aggregate): neighbouring throws an exception if the field requested is not present

--- src/commonMain/kotlin/Aggregate.kt
+++ src/commonMain/kotlin/Aggregate.kt
@@ -1,10 +1,12 @@
 class Aggregate {
     // nbr
-    fun <X : Any> neighbouring(event: X) : FieldImpl<Any> = Environment.fields.retrieveField(event)
+    fun <X : Any> neighbouring(event: X): FieldImpl<Any> =
+        Environment.fields.retrieveField(event) ?:
+        throw IllegalArgumentException("No field found of the selected event")
     // rep
-    /*fun repeating() = println("repeating")
+    fun <X> repeating(initial: X, repeat: (X) -> X): X  = TODO()
     // share
-    fun sharing() = println("sharing")*/
+    /*fun sharing() = println("sharing")*/
 }
 
 fun aggregate(init: Aggregate.() -> Unit): Aggregate = Aggregate().apply(init)
