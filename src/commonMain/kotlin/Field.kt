519b0fadbd73d1ec5fbeb59e797f2bc976701e29
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2022-10-10 16:55:09 +0200

    feat(field): added function to get a value by id

--- src/commonMain/kotlin/Field.kt
+++ src/commonMain/kotlin/Field.kt
@@ -2,7 +2,8 @@ interface Field<out T>
 
 class FieldImpl<T> : Field<T> {
     val field: MutableMap<Int, T> = mutableMapOf()
-     fun addElement(id: Int, value: T): T? = field.put(id, value)
+    fun addElement(id: Int, value: T): T? = field.put(id, value)
+    fun getById(id: Int): T? = field[id]
     override fun toString(): String {
         return "FieldImpl(field=$field)"
     }
