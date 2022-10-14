faa305fa2ca740e640867f70a71ef017806c7dbd
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2022-10-14 16:12:29 +0200

    test(field): added test when trying to retrieve a field by an id that does not exist

--- src/commonTest/kotlin/FieldTest.kt
+++ src/commonTest/kotlin/FieldTest.kt
@@ -1,10 +1,12 @@
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertNull
 import kotlin.test.assertTrue
 
 class FieldTest {
     private val field: FieldImpl<String> = FieldImpl()
     private val testId: Int = 0
+    private val wrongId: Int = 42
     private val testValue: String = "test"
 
     @Test
@@ -20,5 +22,11 @@ class FieldTest {
     fun getValueById(){
         field.addElement(testId, testValue)
         assertEquals(testValue, field.getById(testId))
+    }
+
+    @Test
+    fun getValueByWrongId(){
+        field.addElement(testId, testValue)
+        assertNull(field.getById(wrongId))
     }
 }
