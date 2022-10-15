85527daa266b9d6c13303aa9c9e0e8e44108d095
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2022-10-15 17:47:53 +0200

    feat(main): calling repeating more time to check the final result

--- src/commonMain/kotlin/Main.kt
+++ src/commonMain/kotlin/Main.kt
@@ -2,10 +2,13 @@ fun main() {
 
     val fields = Environment.localFields
     val f: (Int) -> Int = { it * 2 }
+    val k: (String) -> Int = { it.length }
     println(fields)
     // DSL functions
     aggregate {
         repeating(1, f)
+        repeating(1, f)
+        repeating("casa", k)
     }
     println(fields)
 }
