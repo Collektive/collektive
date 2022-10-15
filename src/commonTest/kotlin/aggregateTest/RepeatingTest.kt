06be2a4117599db10e3f10c2e90b67261037c0e1
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2022-10-15 18:01:35 +0200

    test(repeating): added usage of event

--- src/commonTest/kotlin/aggregateTest/RepeatingTest.kt
+++ src/commonTest/kotlin/aggregateTest/RepeatingTest.kt
@@ -3,17 +3,19 @@ package aggregateTest
 import Environment.deviceId
 import Environment.localFields
 import aggregate
+import event.EventImpl
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class RepeatingTest {
     private val initialValue: Int = 1
-    private val event: (Int) -> Int = { it * 2}
+    private val type: (Int) -> Int = { it * 2 }
+    private val event = EventImpl(type)
 
     @Test
-    fun repeatingWithInitialValue(){
+    fun repeatingWithInitialValue() {
         aggregate {
-            repeating(initialValue, event)
+            repeating(initialValue, type)
         }
         assertEquals(2, localFields.retrieveField(event).getById(deviceId))
     }
@@ -21,7 +23,7 @@ class RepeatingTest {
     @Test
     fun repeatingWithLocalValue(){
         aggregate {
-            repeating(initialValue, event)
+            repeating(initialValue, type)
         }
         assertEquals(4, localFields.retrieveField(event).getById(deviceId))
     }
