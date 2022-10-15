3aad6fb4ff190c603052f40b6e019fb3e1209cae
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2022-10-15 18:00:57 +0200

    test(fields): added usage of event

--- src/commonTest/kotlin/FieldsTest.kt
+++ src/commonTest/kotlin/FieldsTest.kt
@@ -1,3 +1,4 @@
+import event.EventImpl
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
@@ -6,27 +7,27 @@ class FieldsTest {
     private val fields = LocalFields<Any>()
     @Test
     fun addField(){
-        fields.addField(Int)
+        fields.addField(EventImpl(Int))
         assertEquals(1, fields.retrieveAllFields().size)
     }
 
     @Test
     fun addFieldOFDifferentType(){
-        fields.addField(Int)
-        fields.addField(String)
+        fields.addField(EventImpl(Int))
+        fields.addField(EventImpl(String))
         assertEquals(2, fields.retrieveAllFields().size)
     }
 
     @Test
     fun retrieveField() {
-        fields.addField(Int)
-        assertNotNull(fields.retrieveField(Int))
+        fields.addField(EventImpl(Int))
+        assertNotNull(fields.retrieveField(EventImpl(Int)))
     }
 
     @Test
     fun modifyField() {
-        fields.addField(String)
-        fields.retrieveField(String).addElement(0, "test")
-        assertNotNull(fields.retrieveField(String).getById(0))
+        fields.addField(EventImpl(String))
+        fields.retrieveField(EventImpl(String)).addElement(0, "test")
+        assertNotNull(fields.retrieveField(EventImpl(String)).getById(0))
     }
 }
