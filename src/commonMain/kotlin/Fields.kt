af7f6e24faed780cc81a3d5c4f6c88b86cd59e77
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2022-10-15 17:45:07 +0200

    fix(fields): added the usage of event

--- src/commonMain/kotlin/Fields.kt
+++ src/commonMain/kotlin/Fields.kt
@@ -1,14 +1,16 @@
+import event.Event
+
 sealed class Fields<X : Any> {
     abstract val fields: MutableMap<Int, Field<X>>
 
     fun retrieveAllFields(): List<Field<X>> = fields.values.toList()
 
-    fun addField(event: X): Field<X>? = fields.put(event::class.hashCode(), FieldImpl())
+    fun addField(event: Event<X>): Field<X>? = fields.put(event.identifier(), FieldImpl())
 
-    fun retrieveField(event: X): Field<X> = fields[event::class.hashCode()] ?:
-        throw IllegalArgumentException("No field found of the selected event")
+    fun retrieveField(event: Event<X>): Field<X> = fields[event.identifier()]
+        ?: throw IllegalArgumentException("No field found of the selected event")
 
-    fun isFieldPresent(event: X): Boolean = fields.containsKey(event::class.hashCode())
+    fun isFieldPresent(event: Event<X>): Boolean = fields.containsKey(event.identifier())
 }
 
 data class LocalFields<X : Any>(override val fields: MutableMap<Int, Field<X>> = mutableMapOf()) : Fields<X>()
