7a123f8ff5dcd5f14de519783f64a112381b53fe
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2022-10-15 10:49:40 +0200

    feat(field): get by id throws an exception if the key does not exist

--- src/commonMain/kotlin/Field.kt
+++ src/commonMain/kotlin/Field.kt
@@ -1,12 +1,13 @@
 interface Field<T : Any> {
     fun addElement(id: Int, value: T): T?
-    fun getById(id: Int): T?
+    fun getById(id: Int): T
 }
 
 class FieldImpl<T : Any> : Field<T> {
     val field: MutableMap<Int, T> = mutableMapOf()
     override fun addElement(id: Int, value: T): T? = field.put(id, value)
-    override fun getById(id: Int): T? = field[id]
+    override fun getById(id: Int): T = field[id]
+        ?: throw IllegalArgumentException("No value found of the selected event")
     override fun toString(): String {
         return "FieldImpl(field=$field)"
     }
