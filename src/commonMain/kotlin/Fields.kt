fdd1672d94766bb240889a7eb9a4ff81e5e11294
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2022-10-16 18:49:57 +0200

    fix(fields): added code to use event class

--- src/commonMain/kotlin/Fields.kt
+++ src/commonMain/kotlin/Fields.kt
@@ -1,17 +1,19 @@
 import event.Event
 
 sealed class Fields<X : Any> {
-    abstract val fields: MutableMap<Int, Field<X>>
+    abstract val fields: MutableMap<String, Field<X>>
 
     fun retrieveAllFields(): List<Field<X>> = fields.values.toList()
 
-    fun addField(event: Event<X>): Field<X>? = fields.put(event.identifier(), FieldImpl())
+    fun addField(event: Event<X>): Field<X>? {
+        return fields.put(event.identifier, FieldImpl())
+    }
 
-    fun retrieveField(event: Event<X>): Field<X> = fields[event.identifier()]
-        ?: throw IllegalArgumentException("No field found of the selected event")
+    fun retrieveField(event: Event<X>): Field<X> = fields[event.identifier]
+            ?: throw IllegalArgumentException("No field found of the selected event")
 
-    fun isFieldPresent(event: Event<X>): Boolean = fields.containsKey(event.identifier())
+    fun isFieldPresent(event: Event<X>): Boolean = fields.containsKey(event.identifier)
 }
 
-data class LocalFields<X : Any>(override val fields: MutableMap<Int, Field<X>> = mutableMapOf()) : Fields<X>()
-data class GlobalFields<X : Any>(override val fields: MutableMap<Int, Field<X>> = mutableMapOf()) : Fields<X>()
+data class LocalFields<X : Any>(override val fields: MutableMap<String, Field<X>> = mutableMapOf()) : Fields<X>()
+data class GlobalFields<X : Any>(override val fields: MutableMap<String, Field<X>> = mutableMapOf()) : Fields<X>()
