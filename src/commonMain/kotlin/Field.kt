c7d7d17f9c25f111ea5080237bbd9ea3938ae204
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2022-10-24 13:30:30 +0200

    feat(field): added method to get the size of the field

--- src/commonMain/kotlin/Field.kt
+++ src/commonMain/kotlin/Field.kt
@@ -1,6 +1,7 @@
 interface Field<T : Any> {
     fun addElement(id: Int, value: T): T?
     fun getById(id: Int): T
+    fun fieldSize(): Int
 }
 
 class FieldImpl<T : Any> : Field<T> {
@@ -8,6 +9,7 @@ class FieldImpl<T : Any> : Field<T> {
     override fun addElement(id: Int, value: T): T? = field.put(id, value)
     override fun getById(id: Int): T = field[id]
         ?: throw IllegalArgumentException("No value found of the selected event")
+    override fun fieldSize(): Int = field.size
     override fun toString(): String {
         return "FieldImpl(field=$field)"
     }
