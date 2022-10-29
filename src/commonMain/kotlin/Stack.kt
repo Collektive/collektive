421640cc3ff8a5cf3db5cf0e6b1a5c850305a78d
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2022-10-29 15:59:32 +0200

    feat(stack): created stack implementation, which also handles the current path updates

--- src/commonMain/kotlin/Stack.kt
+++ src/commonMain/kotlin/Stack.kt
@@ -1,3 +1,13 @@
 interface Stack {
-    fun <X> inNewFrame(frameId: Any, compute: (Path) -> X): X = compute(Path())
+    fun <X> inNewFrame(frameId: Any, compute: (Path) -> X): X
+}
+
+class StackImpl : Stack {
+    private val path: Path = PathImpl()
+    override fun <X> inNewFrame(frameId: Any, compute: (Path) -> X): X {
+        path.addToken(frameId)
+        return compute(path).also {
+            path.closeToken(frameId)
+        }
+    }
 }
