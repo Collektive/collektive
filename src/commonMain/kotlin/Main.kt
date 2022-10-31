354cdfcf2409c7f8a3e2fa83bdb3ae3f6a03c997
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2022-10-31 13:29:27 +0100

    chore: written main that can actually use the new implementation of the dsl operators

--- src/commonMain/kotlin/Main.kt
+++ src/commonMain/kotlin/Main.kt
@@ -1,13 +1,11 @@
 fun main() {
 
-    val fields = Environment.globalFields
     val f: (Int) -> Int = { it * 2 }
-    val k: (String) -> Int = { it.length }
+    //val k: (String) -> Int = { it.length }
     // DSL functions
     aggregate {
-        //repeating(1, f)
-        neighbouring(f(1))
-        neighbouring(k("test"))
+        repeating(1, f)
+        //neighbouring(f(1))
+        //neighbouring(k("test"))
     }
-    println(fields)
 }
