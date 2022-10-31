fa6ef5132a0de359cde775b67370151147b88309
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2022-10-31 13:27:21 +0100

    fix(field): removed field size from the functions

--- src/commonMain/kotlin/Field.kt
+++ src/commonMain/kotlin/Field.kt
@@ -2,7 +2,6 @@ interface Field<ID, out T> {
     val local: Pair<ID, T>
     fun toMap(): Map<ID, T>
     operator fun get(id: ID): T
-    fun fieldSize(): Int
 }
 
 class FieldImpl<ID, out T>(override val local: Pair<ID, T>, messages: Map<ID, T> = emptyMap()) : Field<ID, T> {
@@ -10,7 +9,6 @@ class FieldImpl<ID, out T>(override val local: Pair<ID, T>, messages: Map<ID, T>
     override fun get(id: ID): T = field[id]
         ?: throw IllegalArgumentException("No value found for the specified id")
     override fun toMap(): Map<ID, T> = field.toMap()
-    override fun fieldSize(): Int = field.size
     override fun toString(): String {
         return "FieldImpl(field=$field)"
     }
