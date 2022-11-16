c706a9b4b876c08aca2a1ba418c8a3f19d094fc2
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2022-11-16 09:38:16 +0100

    feat(compiler-plugin): added the command line option to enable the compiler plugin

---
+++ compiler-plugin/src/main/kotlin/com/etronetti/CommandLineProcessorImpl.kt
@@ -0,0 +1,38 @@
+package com.etronetti
+
+import com.google.auto.service.AutoService
+import org.jetbrains.kotlin.compiler.plugin.AbstractCliOption
+import org.jetbrains.kotlin.compiler.plugin.CliOption
+import org.jetbrains.kotlin.compiler.plugin.CommandLineProcessor
+import org.jetbrains.kotlin.config.CompilerConfiguration
+import org.jetbrains.kotlin.config.CompilerConfigurationKey
+
+@AutoService(CommandLineProcessor::class)
+class CommandLineProcessorImpl : CommandLineProcessor {
+    companion object {
+        private const val OPTION_ENABLED = "enabled"
+        val ARG_ENABLED = CompilerConfigurationKey<Boolean>(OPTION_ENABLED)
+    }
+
+    override val pluginId: String = BuildConfig.KOTLIN_PLUGIN_ID
+
+    override val pluginOptions: Collection<CliOption> = listOf(
+        CliOption(
+            optionName = OPTION_ENABLED,
+            valueDescription = "bool <true | false>",
+            description = "If the DebugLog annotation should be applied",
+            required = false,
+        ),
+    )
+
+    override fun processOption(
+        option: AbstractCliOption,
+        value: String,
+        configuration: CompilerConfiguration
+    ) {
+        return when (option.optionName) {
+            OPTION_ENABLED -> configuration.put(ARG_ENABLED, value.toBoolean())
+            else -> throw IllegalArgumentException("Unexpected config option ${option.optionName}")
+        }
+    }
+}
