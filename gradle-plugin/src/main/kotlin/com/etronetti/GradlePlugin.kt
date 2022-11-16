305a5de58ef170b62f0e703caf01e129041bd9ae
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2022-11-16 09:43:30 +0100

    feat(gradle-plugin): created connection to the actual compiler plugin

---
+++ gradle-plugin/src/main/kotlin/com/etronetti/GradlePlugin.kt
@@ -0,0 +1,36 @@
+package com.etronetti
+
+import org.gradle.api.Project
+import org.gradle.api.provider.Provider
+import org.jetbrains.kotlin.gradle.plugin.KotlinCompilation
+import org.jetbrains.kotlin.gradle.plugin.KotlinCompilerPluginSupportPlugin
+import org.jetbrains.kotlin.gradle.plugin.SubpluginArtifact
+import org.jetbrains.kotlin.gradle.plugin.SubpluginOption
+
+class GradlePlugin : KotlinCompilerPluginSupportPlugin {
+    override fun apply(target: Project): Unit = with(target) {
+        extensions.create("kotlinIrPlugin", GradleExtension::class.java)
+    }
+
+    override fun isApplicable(kotlinCompilation: KotlinCompilation<*>): Boolean = true
+
+    override fun getCompilerPluginId(): String = BuildConfig.KOTLIN_PLUGIN_ID
+
+    override fun getPluginArtifact(): SubpluginArtifact = SubpluginArtifact(
+        groupId = BuildConfig.KOTLIN_PLUGIN_GROUP,
+        artifactId = BuildConfig.KOTLIN_PLUGIN_NAME,
+        version = BuildConfig.KOTLIN_PLUGIN_VERSION
+    )
+
+    override fun applyToCompilation(
+        kotlinCompilation: KotlinCompilation<*>
+    ): Provider<List<SubpluginOption>> {
+        val project = kotlinCompilation.target.project
+        val extension = project.extensions.getByType(GradleExtension::class.java)
+        return project.provider {
+            listOf(
+                SubpluginOption(key = "enabled", value = extension.enabled.toString()),
+            )
+        }
+    }
+}
