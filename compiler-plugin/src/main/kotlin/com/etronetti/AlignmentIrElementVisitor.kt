41141b5f4cc37ec633dfa7de4fa91b9cf3b943b1
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2022-11-17 11:54:56 +0100

    feat(compiler-plugin): created visitor used to search for the alignment function

---
+++ compiler-plugin/src/main/kotlin/com/etronetti/AlignmentIrElementVisitor.kt
@@ -0,0 +1,35 @@
+package com.etronetti
+
+import org.jetbrains.kotlin.ir.IrElement
+import org.jetbrains.kotlin.ir.declarations.IrFunction
+import org.jetbrains.kotlin.ir.visitors.IrElementVisitor
+
+/**
+ * Class that visit all the children of the IR, looking for the function
+ * that is responsible to handle the alignment.
+ */
+class AlignmentIrElementVisitor(
+    private val elements: MutableList<IrFunction>
+) : IrElementVisitor<Unit, Nothing?> {
+    private val alignmentFunctionName: String = "alignedOn"
+
+    // Visit all the children of the root element
+    override fun visitElement(element: IrElement, data: Nothing?) {
+        element.acceptChildren(this, data)
+    }
+
+    // Looking for the function declaration with the target names
+    override fun visitFunction(declaration: IrFunction, data: Nothing?) {
+        if (declaration.name.toString() == alignmentFunctionName){
+            elements.add(declaration)
+        }
+        super.visitFunction(declaration, data)
+    }
+}
+
+/**
+ * Retrieve the function that matches the target function name
+ */
+fun collect(element: IrElement) = buildList {
+    element.accept(AlignmentIrElementVisitor(this), null)
+}
