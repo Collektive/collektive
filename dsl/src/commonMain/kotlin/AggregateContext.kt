c22657f5ffc757f93adff5e2012c48d7efdc34a5
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2022-11-17 12:09:21 +0100

    feat(aggregate): updated dsl function to use the stack object and removed stack handling

--- dsl/src/commonMain/kotlin/AggregateContext.kt
+++ dsl/src/commonMain/kotlin/AggregateContext.kt
@@ -1,7 +1,5 @@
 import stack.Path
-import stack.Stack
-import stack.StackImpl
-import stack.Token
+import stack.Stack.currentPath
 
 class AggregateContext(
     private val localId: ID,
@@ -10,42 +8,36 @@ class AggregateContext(
 
     private val state: MutableMap<Path, Any> = mutableMapOf()
     private val toBeSent: MutableMap<Path, Any?> = mutableMapOf()
-    private val stack: Stack = StackImpl()
 
     fun messagesToSend(): Map<Path, *> = toBeSent.toMap()
     fun newState(): Map<Path, *> = state.toMap()
 
     // nbr
     fun neighbouring(type: Any?): Field<ID, Any?> {
-        return alignedOn(Token.NEIGHBOURING) { here ->
-            toBeSent[here] = type
-            val messages = messagesAt(here)
-            FieldImpl(Pair(localId, type), messages)
-        }
+        toBeSent[currentPath()] = type
+        val messages = messagesAt(currentPath())
+        return FieldImpl(Pair(localId, type), messages)
     }
 
     // rep
     @Suppress("UNCHECKED_CAST")
     fun <X,Y : Any> repeating(initial: X, repeat: (X) -> Y): Y {
-        return alignedOn(Token.REPEATING) { here ->
-            val res = if (previousState.containsKey(here)) repeat(previousState[here] as X) else repeat(initial)
-            state[here] = res
-            res
-        }
+        val res = if (previousState.containsKey(currentPath())) repeat(previousState[currentPath()] as X) else repeat(initial)
+        state[currentPath()] = res
+        return res
     }
 
     // share
-    fun <X, Y: Any> sharing(initial: X, body: (Field<ID, Any?>) -> Y): Y = alignedOn(Token.SHARING) { here ->
-        val messages = messagesAt(here)
-        val previous = if (previousState.containsKey(here)) previousState[here] else initial
+    fun <X, Y: Any> sharing(initial: X, body: (Field<ID, Any?>) -> Y): Y {
+        val messages = messagesAt(currentPath())
+        val previous = if (previousState.containsKey(currentPath())) previousState[currentPath()] else initial
         val subject = FieldImpl(Pair(localId, previous), messages)
-        body(subject).also {
-            toBeSent[here] = it
+        return body(subject).also {
+            toBeSent[currentPath()] = it
         }
     }
 
     private fun messagesAt(path: Path): Map<ID, *> = messages[path] ?: emptyMap<ID, Any>()
-    private fun <X: Any, Y> alignedOn(token: X, operation: (Path) -> Y) = stack.inNewFrame(token, operation)
 
     data class AggregateResult<X>(val result: X, val toSend: Map<Path, *>, val newState: Map<Path, *>)
 }
