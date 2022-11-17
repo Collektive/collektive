68d818b55e0cf9d855f990d3455dfe9e37932fa0
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2022-11-17 10:32:34 +0100

    chore(gradle-plugin): added documentation to gradle extension

--- gradle-plugin/src/main/kotlin/com/etronetti/GradleExtension.kt
+++ gradle-plugin/src/main/kotlin/com/etronetti/GradleExtension.kt
@@ -3,6 +3,10 @@ package com.etronetti
 import org.gradle.api.model.ObjectFactory
 import org.gradle.api.provider.Property
 
+/**
+ * Creating gradle extension which is used to define a property that can be
+ * used to enable or disable the plugin.
+ */
 open class GradleExtension(objects: ObjectFactory) {
     private val enabledProperty: Property<Boolean> = objects.property(Boolean::class.java)
         .apply { convention(true) }
