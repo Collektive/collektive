a9673a26243291b2e9d6ba56074c5de0c5cdc335
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2022-11-17 17:00:27 +0100

    test(stack): fixed stack tests

--- dsl/src/commonTest/kotlin/stack/StackTest.kt
+++ dsl/src/commonTest/kotlin/stack/StackTest.kt
@@ -4,17 +4,18 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class StackTest {
-    private val stack: Stack = StackImpl()
-    private val double: (Int) -> Int = { it * 2 }
-    private val initValue: Int = 1
-
+    private val token: String = "token"
 
     @Test
-    fun openNewFrame() {
-        val res = stack.inNewFrame(Token.REPEATING) {
-            assertEquals(Token.REPEATING.toString(), it.currentList().last())
-            double(initValue)
-        }
-        assertEquals(2, res)
+    fun addTokenToStack() {
+        Stack.addToken(token)
+        assertEquals(token, Stack.currentPath().path.last())
     }
+
+    @Test
+    fun clearStack() {
+        Stack.clearStack()
+        assertEquals(emptyList(), Stack.currentPath().path)
+    }
+
 }
