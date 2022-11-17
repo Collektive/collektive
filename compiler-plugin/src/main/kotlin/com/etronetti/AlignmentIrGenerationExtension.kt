3655184012417d1bd6db5ccd351f3134d86d011f
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2022-11-17 11:56:18 +0100

    feat(compiler-plugin): added the research to the alignment function

--- compiler-plugin/src/main/kotlin/com/etronetti/AlignmentIrGenerationExtension.kt
+++ compiler-plugin/src/main/kotlin/com/etronetti/AlignmentIrGenerationExtension.kt
@@ -4,10 +4,14 @@ import org.jetbrains.kotlin.backend.common.extensions.IrGenerationExtension
 import org.jetbrains.kotlin.backend.common.extensions.IrPluginContext
 import org.jetbrains.kotlin.ir.declarations.IrModuleFragment
 
-class IrGenerationExtensionImpl: IrGenerationExtension {
+/**
+ * The generation extension is used to register the transformer plugin, which is going to modify
+ * the IR using the function responsible for the alignment.
+ */
+class AlignmentIrGenerationExtension: IrGenerationExtension {
     override fun generate(moduleFragment: IrModuleFragment, pluginContext: IrPluginContext) {
         // Function that is responsible to handle the alignment
         val alignOnFunction = collect(moduleFragment).first()
-        moduleFragment.transform(TransformerImpl(pluginContext, alignOnFunction), null)
+        moduleFragment.transform(AlignmentIrElementTransformer(pluginContext, alignOnFunction), null)
     }
 }
