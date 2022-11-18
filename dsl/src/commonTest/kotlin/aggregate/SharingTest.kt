3cd59d2a1e90e74f26c5d0c27ba55f92d94bcd31
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2022-11-18 18:33:04 +0100

    test(aggregate): added tests for sharing

---
+++ dsl/src/commonTest/kotlin/aggregate/SharingTest.kt
@@ -0,0 +1,44 @@
+package aggregate
+
+import Field
+import ID
+import Network
+import NetworkImpl
+import aggregate
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+class SharingTest {
+    private val initialValue: Int = 1
+    private val initialValue2: Int = 2
+    private val findMax: (Field<ID, Int>) -> Int = { e -> e.toMap().maxBy { it.value }.value }
+
+    @Test
+    fun firstTimeSharing(){
+        aggregate {
+            val res = sharing(initialValue, findMax)
+            assertEquals(1, res)
+        }
+    }
+
+    @Test
+    fun sharingWithTwoAlignedDevices(){
+        val network: Network = NetworkImpl()
+
+        var i = 0
+        val condition: () -> Boolean = { i++ < 1 }
+
+        // Device 1
+        aggregate(condition, network) {
+            sharing(initialValue, findMax)
+        }
+
+        i = 0
+        // Device 2
+        aggregate(condition, network) {
+            val res = sharing(initialValue2, findMax)
+            assertEquals(2, res)
+        }
+    }
+}
