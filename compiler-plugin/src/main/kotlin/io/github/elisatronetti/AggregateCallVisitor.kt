6581156d8d318e3e3c90c09f2ea8c59c18cf3f53
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2022-12-09 16:33:33 +0100

    feat(compiler-plugin): added visitor that collects all the function call inside of aggregate

---
+++ compiler-plugin/src/main/kotlin/io/github/elisatronetti/AggregateCallVisitor.kt
@@ -0,0 +1,31 @@
+package io.github.elisatronetti
+
+import org.jetbrains.kotlin.ir.IrElement
+import org.jetbrains.kotlin.ir.expressions.IrCall
+import org.jetbrains.kotlin.ir.visitors.IrElementVisitor
+
+/**
+ * Class that visit all the children of the aggregate, looking for the all the
+ * function calls.
+ */
+class AggregateCallVisitor(
+        private val elements: MutableList<IrCall>
+) : IrElementVisitor<Unit, Nothing?> {
+
+    // Visit all the children of the root element
+    override fun visitElement(element: IrElement, data: Nothing?) {
+        element.acceptChildren(this, data)
+    }
+
+    override fun visitCall(expression: IrCall, data: Nothing?) {
+        if (expression !in elements) elements.add(expression)
+        super.visitCall(expression, data)
+    }
+}
+
+/**
+ * Retrieve the function calls found in aggregate.
+ */
+fun collectCalls(element: IrElement) = buildList {
+    element.accept(AggregateCallVisitor(this), null)
+}
