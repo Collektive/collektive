36ace3a44adc24237ee30c439283634accdaf35a
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2022-12-09 16:43:55 +0100

    feat(main): created basic main to easily test the dsl

--- dsl/src/commonMain/kotlin/Main.kt
+++ dsl/src/commonMain/kotlin/Main.kt
@@ -1,20 +1,7 @@
-import field.min
-
 fun main() {
-    val network: Network = NetworkImpl()
 
-    val f: (Int) -> Int = { it * 2 }
-    var i = 0
-    val condition: () -> Boolean = { i++ < 2 }
-
-    // Device 1
-    aggregate(condition, network) {
-        neighbouring(f(3))
-    }
-
-    i = 0
-    // Device 2
-    aggregate(condition, network) {
-        println(neighbouring(f(2)).min())
+    aggregate {
+        fun testFunction(value: Int) = neighbouring(value * 2)
+        testFunction(3)
     }
 }
