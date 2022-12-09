0dc7dfd066622b8f19edbb7b408def824f4b4924
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2022-12-09 16:32:36 +0100

    feat(compiler-plugin): added trasformer that activet the research of all the function called inside of aggregate

--- compiler-plugin/src/main/kotlin/io/github/elisatronetti/AggregateIrElementTransformer.kt
+++ compiler-plugin/src/main/kotlin/io/github/elisatronetti/AggregateIrElementTransformer.kt
@@ -1,49 +1,32 @@
 package io.github.elisatronetti
 
 import org.jetbrains.kotlin.backend.common.extensions.IrPluginContext
-import org.jetbrains.kotlin.backend.jvm.ir.receiverAndArgs
-import org.jetbrains.kotlin.ir.IrStatement
 import org.jetbrains.kotlin.ir.declarations.IrClass
 import org.jetbrains.kotlin.ir.declarations.IrFunction
 import org.jetbrains.kotlin.ir.expressions.*
-import org.jetbrains.kotlin.ir.util.dump
-import org.jetbrains.kotlin.ir.util.getPropertyGetter
 import org.jetbrains.kotlin.ir.visitors.IrElementTransformerVoid
 
 /**
  * Looking for the aggregate function call, find all its function call
- * childern and transform them.
+ * children and transform them.
  */
-class AlignmentIrElementTransformer(
+class AggregateIrElementTransformer(
     private val pluginContext: IrPluginContext,
-    private val stampa: IrFunction,
+    private val alignOnFunction: IrFunction,
     private val aggregateContext: IrClass
 ) : IrElementTransformerVoid() {
+    private val aggregateFunctionCallName: String = "aggregate"
 
     override fun visitCall(expression: IrCall): IrExpression {
-        if (expression.symbol.owner.name.asString() == "aggregate") {
-            val calls = collectCalls(expression)
-            for (call in calls) {
-                call.transform(ModifyAlignTrasformer(pluginContext, stampa, aggregateContext), null)
+        if (expression.symbol.owner.name.asString() == aggregateFunctionCallName) {
+            collectCalls(expression).forEach {
+                // Modify all irCall that are children of aggregate
+                it.transform(
+                    AlignmentIrElementTransformer(pluginContext, alignOnFunction, aggregateContext),
+                    null
+                )
             }
         }
-        //val aggregateReceiver = expression.receiverAndArgs().find { aggregateContext.contains(it) }
-        /*if (aggregateReceiver != null) {
-            //println(expression.dump())
-            //println("---------------")
-        }*/
-
-        /*aggregateReceiver.forEach {
-            println( it.dump())
-        }
-        println("-----------------")*/
-
-        /*if (aggregateReceiver != null) {
-            // IrExpression devono essere le tre call in fila
-            // aggregateReceiver.alignRaw(expression.symbol.owner.name).let { call() }.also { dealign() }
-            irAlign(aggregateReceiver, expression.symbol.owner., )
-            // chiama align passandogli la funzione che sto per chiamare
-        }*/
         return super.visitCall(expression)
     }
 }
