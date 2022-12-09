508a9324726e0f147e8f7befdba6fd6d1f844d17
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2022-12-09 16:42:18 +0100

    fix(aggregate-context): adapted aggregate context to the new version of stack

--- dsl/src/commonMain/kotlin/AggregateContext.kt
+++ dsl/src/commonMain/kotlin/AggregateContext.kt
@@ -1,13 +1,14 @@
 import field.Field
 import field.FieldImpl
 import stack.Path
-import stack.Stack.currentPath
+import stack.Stack
 
 class AggregateContext(
     private val localId: ID,
     private val messages: Map<Path, Map<ID, *>>,
     private val previousState: Map<Path, *>) {
 
+    private val stack: Stack<Any> = Stack()
     private val state: MutableMap<Path, Any> = mutableMapOf()
     private val toBeSent: MutableMap<Path, Any?> = mutableMapOf()
 
@@ -16,26 +17,32 @@ class AggregateContext(
 
     // nbr
     fun <X> neighbouring(type: X): Field<X> {
-        toBeSent[currentPath()] = type
-        val messages = messagesAt(currentPath())
+        val currentPath = stack.currentPath()
+        toBeSent[currentPath] = type
+        println(currentPath)
+        val messages = messagesAt(stack.currentPath())
         return FieldImpl(Pair(localId, type), messages)
     }
 
     // rep
     @Suppress("UNCHECKED_CAST")
     fun <X,Y : Any> repeating(initial: X, repeat: (X) -> Y): Y {
-        val res = if (previousState.containsKey(currentPath())) repeat(previousState[currentPath()] as X) else repeat(initial)
-        state[currentPath()] = res
+        val res = if (previousState.containsKey(stack.currentPath())) repeat(previousState[stack.currentPath()] as X) else repeat(initial)
+        val currentPath = stack.currentPath()
+        println(currentPath)
+        state[currentPath] = res
         return res
     }
 
     // share
     fun <X, Y: Any?> sharing(initial: X, body: (Field<X>) -> Y): Y {
-        val messages = messagesAt(currentPath())
-        val previous = if (previousState.containsKey(currentPath())) (previousState[currentPath()]) else initial
+        val messages = messagesAt(stack.currentPath())
+        val previous = if (previousState.containsKey(stack.currentPath())) (previousState[stack.currentPath()]) else initial
         val subject = FieldImpl<X>(Pair(localId, previous), messages)
         return body(subject).also {
-            toBeSent[currentPath()] = it
+            val currentPath = stack.currentPath()
+            println(currentPath)
+            toBeSent[currentPath] = it
         }
     }
 
