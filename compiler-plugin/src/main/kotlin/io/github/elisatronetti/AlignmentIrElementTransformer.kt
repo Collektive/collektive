c1dfee4d87cfd04271524946578cbfab1ad468a1
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2022-12-21 18:01:01 +0100

    chore(compiler-plugin): refactor of the compiler plugin lambda creation

--- compiler-plugin/src/main/kotlin/io/github/elisatronetti/AlignmentIrElementTransformer.kt
+++ compiler-plugin/src/main/kotlin/io/github/elisatronetti/AlignmentIrElementTransformer.kt
@@ -2,36 +2,22 @@ package io.github.elisatronetti
 
 import org.jetbrains.kotlin.backend.common.extensions.IrPluginContext
 import org.jetbrains.kotlin.backend.common.ir.allParameters
-import org.jetbrains.kotlin.backend.common.ir.setDeclarationsParent
-import org.jetbrains.kotlin.backend.common.lower.DeclarationIrBuilder
 import org.jetbrains.kotlin.backend.common.lower.createIrBuilder
-import org.jetbrains.kotlin.backend.jvm.ir.defaultValue
 import org.jetbrains.kotlin.backend.jvm.ir.receiverAndArgs
 import org.jetbrains.kotlin.builtins.StandardNames
 import org.jetbrains.kotlin.descriptors.DescriptorVisibilities
-import org.jetbrains.kotlin.descriptors.impl.FunctionExpressionDescriptor
 import org.jetbrains.kotlin.ir.IrElement
-import org.jetbrains.kotlin.ir.ObsoleteDescriptorBasedAPI
-import org.jetbrains.kotlin.ir.backend.js.utils.invokeFunForLambda
 import org.jetbrains.kotlin.ir.builders.*
-import org.jetbrains.kotlin.ir.builders.declarations.IrFunctionBuilder
 import org.jetbrains.kotlin.ir.builders.declarations.buildFun
 import org.jetbrains.kotlin.ir.declarations.*
 import org.jetbrains.kotlin.ir.expressions.*
-import org.jetbrains.kotlin.ir.expressions.impl.IrBlockBodyImpl
 import org.jetbrains.kotlin.ir.expressions.impl.IrFunctionExpressionImpl
-import org.jetbrains.kotlin.ir.expressions.impl.IrReturnImpl
-import org.jetbrains.kotlin.ir.expressions.impl.IrSyntheticBodyImpl
-import org.jetbrains.kotlin.ir.symbols.impl.IrSimpleFunctionSymbolImpl
 import org.jetbrains.kotlin.ir.types.IrType
 import org.jetbrains.kotlin.ir.types.isUnit
 import org.jetbrains.kotlin.ir.types.typeWith
 import org.jetbrains.kotlin.ir.util.*
 import org.jetbrains.kotlin.ir.visitors.IrElementTransformerVoid
-import org.jetbrains.kotlin.name.FqName
 import org.jetbrains.kotlin.name.Name
-import org.jetbrains.kotlin.resolve.calls.model.FunctionExpression
-import org.jetbrains.kotlin.utils.addToStdlib.cast
 
 /**
  * This transform the generated IR, creating in the function declaration a new function call,
@@ -40,86 +26,88 @@ import org.jetbrains.kotlin.utils.addToStdlib.cast
 class AlignmentIrElementTransformer(
     private val pluginContext: IrPluginContext,
     private val alignOnFunction: IrFunction,
-    private val aggregateCall: IrCall,
-    private val aggregateContext: IrClass
+    private val aggregateLambdaBody: IrSimpleFunction,
+    private val aggregateClass: IrClass
 ) : IrElementTransformerVoid() {
 
     override fun visitCall(expression: IrCall): IrExpression {
-        val callee: IrFunction = expression.symbol.owner // Declaration of the function call
         val aggregateContextRef: IrExpression? = expression.receiverAndArgs().find {
-            it.type == aggregateContext.defaultType
+            it.type == aggregateClass.defaultType
         }
-        val aggregateLambda = (aggregateCall.getValueArgument(0) as IrFunctionExpression).function
-        fun <T : IrElement> irStatement(body: IrSingleStatementBuilder.() -> T): T =
-            IrSingleStatementBuilder(
-                pluginContext,
-                Scope(expression.symbol),
-                expression.startOffset,
-                expression.endOffset
-            ).build(body)
-        if (aggregateContextRef != null) {
-            val a = irStatement {
-                irCall(alignOnFunction).apply {
-                    val x = pluginContext.irBuiltIns.createIrBuilder(aggregateLambda.symbol).buildLambda(expression)
-                    type = expression.type
-                    putTypeArgument(0, expression.type)
-                    putArgument(alignOnFunction.dispatchReceiverParameter!!, aggregateContextRef)
-                    putValueArgument(
-                        0,
-                        irString(callee.symbol.owner.kotlinFqName.asString())
-                    )
-                    val base = pluginContext.referenceClass(
-                        StandardNames.getFunctionClassId(x.allParameters.size).asSingleFqName()
-                    )
-                        ?: error("function type not found")
 
-                    val type: IrType = base.typeWith(x.allParameters.map { it.type } + x.returnType)
-                    val y = IrFunctionExpressionImpl(
-                        startOffset,
-                        endOffset,
-                        type,
-                        x,
-                        IrStatementOrigin.LAMBDA
-                    )
-                    putValueArgument(1, y)
-                }
-            }
-            return a
+        val aggregateContext: IrExpression = if (aggregateContextRef != null) {
+            aggregateContextRef
         } else {
-                val aggregateContextForse = collectAggregateReference(aggregateContext, expression.symbol.owner)
-                if (aggregateContextForse.isNotEmpty()) {
-                    val a = irStatement {
-                        irCall(alignOnFunction).apply {
-                            val x = pluginContext.irBuiltIns.createIrBuilder(aggregateLambda.symbol).buildLambda(expression)
-                            type = expression.type
-                            putTypeArgument(0, expression.type)
-                            putArgument(alignOnFunction.dispatchReceiverParameter!!, aggregateContextForse.first())
-                            putValueArgument(
-                                0,
-                                irString(callee.symbol.owner.kotlinFqName.asString())
-                            )
-                            val base = pluginContext.referenceClass(
-                                StandardNames.getFunctionClassId(x.allParameters.size).asSingleFqName()
-                            )
-                                ?: error("function type not found")
-
-                            val type: IrType = base.typeWith(x.allParameters.map { it.type } + x.returnType)
-                            val y = IrFunctionExpressionImpl(
-                                startOffset,
-                                endOffset,
-                                type,
-                                x,
-                                IrStatementOrigin.LAMBDA
-                            )
-                            putValueArgument(1, y)
-                        }
-                    }
-                    return a
-                }
+            // Find the aggregate context looking in all the children of expression
+            val childrenAggregateRefs = collectAggregateReference(aggregateClass, expression.symbol.owner)
+            if (childrenAggregateRefs.isNotEmpty()) {
+                childrenAggregateRefs.first()
+            } else {
+                return super.visitCall(expression)
             }
-        return super.visitCall(expression)
+        }
+
+        return irStatement(expression) {
+            buildAlignOnCall(expression, aggregateContext)
+        }
     }
 
+    private fun <T : IrElement> irStatement(expression: IrCall, body: IrSingleStatementBuilder.() -> T): T =
+        IrSingleStatementBuilder(
+            pluginContext,
+            Scope(expression.symbol),
+            expression.startOffset,
+            expression.endOffset
+        ).build(body)
+
+    private fun IrSingleStatementBuilder.buildAlignOnCall(
+        expression: IrCall,
+        aggregateContext: IrExpression
+    ): IrFunctionAccessExpression {
+        return irCall(alignOnFunction).apply {
+            // Set generics type
+            type = expression.type
+            putTypeArgument(0, expression.type)
+            // Set aggregate context
+            putArgument(alignOnFunction.dispatchReceiverParameter!!, aggregateContext)
+            // Set the argument that is going to be push in the stack
+            putValueArgument(
+                0,
+                irString(expression.symbol.owner.kotlinFqName.asString())
+            )
+            // Create the lambda that is going to call expression
+            val lambda = buildLambdaArgument(expression)
+            putValueArgument(1, lambda)
+        }
+    }
+
+    /**
+     * Create a IrFunctionExpression that transform a lambda to an expression
+     * that can be used as argument for another function.
+     */
+    private fun IrBuilderWithScope.buildLambdaArgument(
+        expression: IrCall
+    ): IrFunctionExpressionImpl {
+        val lambda = pluginContext.irBuiltIns.createIrBuilder(aggregateLambdaBody.symbol).buildLambda(expression)
+        val base = pluginContext.referenceClass(
+            StandardNames.getFunctionClassId(lambda.allParameters.size).asSingleFqName()
+        )
+            ?: error("function type not found")
+        val type: IrType = base.typeWith(lambda.allParameters.map { it.type } + lambda.returnType)
+        return IrFunctionExpressionImpl(
+            startOffset,
+            endOffset,
+            type,
+            lambda,
+            IrStatementOrigin.LAMBDA
+        )
+    }
+
+    /**
+     * Build a new lambda, which parent is the IrElement where the function is called.
+     * The body of the lambda calls the expression passed as argument. If expression
+     * has a return type, the lambda body will have the same return type.
+     */
     private fun IrBuilderWithScope.buildLambda(
         expression: IrCall
     ): IrSimpleFunction = pluginContext.irFactory.buildFun {
