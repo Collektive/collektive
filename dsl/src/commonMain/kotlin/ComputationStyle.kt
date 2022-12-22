e22e7ea31e7d6605906fa5c71074a8cbf4284405
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2022-12-22 20:47:13 +0100

    feat(aggregate): the messages are handled by switching the indexes only if necessary

--- dsl/src/commonMain/kotlin/ComputationStyle.kt
+++ dsl/src/commonMain/kotlin/ComputationStyle.kt
@@ -1,9 +1,8 @@
 import stack.Path
-import util.switchIndexes
 
 fun <X> singleCycle(
     localId: ID = IntId(),
-    messages: Map<Path, Map<ID, *>> = emptyMap<Path, Map<ID, Any>>(),
+    messages: Map<ID, Map<Path, *>> = emptyMap<ID, Map<Path, Any>>(),
     state: Map<Path, *> = emptyMap<Path, Any>(),
     compute: AggregateContext.() -> X
 ): AggregateContext.AggregateResult<X> {
@@ -15,13 +14,13 @@ fun <X> singleCycle(
 fun <X> runUntil(
     condition: () -> Boolean,
     network: Network,
-    compute: AggregateContext.() -> X): AggregateContext.AggregateResult<X> {
+    compute: AggregateContext.() -> X
+): AggregateContext.AggregateResult<X> {
     val localId: ID = IntId()
     var state = emptyMap<Path, Any?>()
     var computed: AggregateContext.AggregateResult<X>? = null
     while (condition()) {
-        val messages: Map<Path, Map<ID, *>> = network.receive().switchIndexes()
-        computed = singleCycle(localId, messages, state, compute)
+        computed = singleCycle(localId, network.receive(), state, compute)
         state = computed.newState
         network.send(localId, computed.toSend)
     }
