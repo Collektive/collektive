96d603765f7b5220288e0395debc343e90eb60c1
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2022-12-28 15:16:32 +0100

    feat(compiler-plugin): added extension function used to get the last value argument of a certain function call

--- compiler-plugin/src/main/kotlin/io/github/elisatronetti/utils/common/IrElementUtils.kt
+++ compiler-plugin/src/main/kotlin/io/github/elisatronetti/utils/common/IrElementUtils.kt
@@ -4,10 +4,12 @@ import org.jetbrains.kotlin.backend.common.extensions.IrPluginContext
 import org.jetbrains.kotlin.backend.common.ir.allParameters
 import org.jetbrains.kotlin.builtins.StandardNames
 import org.jetbrains.kotlin.ir.declarations.IrSimpleFunction
+import org.jetbrains.kotlin.ir.expressions.IrCall
 import org.jetbrains.kotlin.ir.expressions.IrExpression
 import org.jetbrains.kotlin.ir.expressions.IrFunctionAccessExpression
 import org.jetbrains.kotlin.ir.types.IrType
 import org.jetbrains.kotlin.ir.types.typeWith
+import java.beans.Expression
 
 /**
  * Put a value argument at the head of the function's arguments.
@@ -30,6 +32,12 @@ internal fun IrFunctionAccessExpression.putTypeArgument(
     this.type = type
     putTypeArgument(0, type)
 }
+
+/**
+ * Get the value argument in the last index available of the function call.
+ */
+internal fun IrCall.getLastValueArgument(): IrExpression? =
+    this.getValueArgument(this.valueArgumentsCount - 1)
 
 internal fun getLambdaType(
     pluginContext: IrPluginContext,
