4de761ecd9664249a321bb8ba111d372c8b5156c
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2022-12-28 14:54:45 +0100

    feat(compiler-plugin): added aggregate function name

--- compiler-plugin/src/main/kotlin/io/github/elisatronetti/utils/common/Name.kt
+++ compiler-plugin/src/main/kotlin/io/github/elisatronetti/utils/common/Name.kt
@@ -2,5 +2,6 @@ package io.github.elisatronetti.utils.common
 
 object Name {
     const val ALIGNED_ON_FUNCTION = "alignedOn"
+    const val AGGREGATE_FUNCTION = "aggregate"
     const val AGGREGATE_CONTEXT_CLASS = "AggregateContext"
 }
