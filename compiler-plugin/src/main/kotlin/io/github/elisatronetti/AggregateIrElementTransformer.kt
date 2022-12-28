58305ac33e2bfcf24b45f11b49319315c80765c6
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2022-12-28 15:06:50 +0100

    fix(compiler-plugin): the tranformer is applied to the aggregate body directly, without visiting the children

--- compiler-plugin/src/main/kotlin/io/github/elisatronetti/AggregateIrElementTransformer.kt
+++ compiler-plugin/src/main/kotlin/io/github/elisatronetti/AggregateIrElementTransformer.kt
@@ -1,34 +1,35 @@
 package io.github.elisatronetti
 
+import io.github.elisatronetti.utils.common.Name
 import org.jetbrains.kotlin.backend.common.extensions.IrPluginContext
-import org.jetbrains.kotlin.backend.jvm.ir.receiverAndArgs
 import org.jetbrains.kotlin.ir.declarations.IrClass
 import org.jetbrains.kotlin.ir.declarations.IrFunction
 import org.jetbrains.kotlin.ir.expressions.*
-import org.jetbrains.kotlin.ir.util.defaultType
 import org.jetbrains.kotlin.ir.visitors.IrElementTransformerVoid
 
 /**
- * Looking for the aggregate function call, find all its function call
- * children and transform them.
+ * Looking for the aggregate function call, which is the one that contains the function calls
+ * and the branches that have to be aligned.
  */
 class AggregateIrElementTransformer(
     private val pluginContext: IrPluginContext,
-    private val alignOnFunction: IrFunction,
-    private val aggregateContext: IrClass
+    private val aggregateContextClass: IrClass,
+    private val alignedOnFunction: IrFunction
 ) : IrElementTransformerVoid() {
-    private val aggregateFunctionCallName: String = "aggregate"
 
     override fun visitCall(expression: IrCall): IrExpression {
-        if (expression.symbol.owner.name.asString() == aggregateFunctionCallName) {
-            val aggregateLambdaBody = (expression.getValueArgument(expression.valueArgumentsCount - 1) as IrFunctionExpression).function
-            collectCalls(expression).forEach {
-                // Modify all irCall that are children of aggregate
-                it.transform(
-                    AlignmentIrElementTransformer(pluginContext, alignOnFunction, aggregateLambdaBody, aggregateContext),
-                    null
-                )
-            }
+        if (expression.symbol.owner.name.asString() == Name.AGGREGATE_FUNCTION) {
+            val aggregateLambdaBody =
+                (expression.getValueArgument(expression.valueArgumentsCount - 1) as IrFunctionExpression).function
+            expression.transform(
+                AlignmentIrElementTransformer(
+                    pluginContext,
+                    alignedOnFunction,
+                    aggregateLambdaBody,
+                    aggregateContextClass
+                ),
+                null
+            )
         }
         return super.visitCall(expression)
     }
