e82cf0be57b2256c926f4482403915f9a7c21f3a
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2023-01-14 16:25:53 +0100

    feat(compiler-plugin): the alignedOn function and the AggregateContext class are found without needing to compile the dsl

--- plugin/compiler-plugin/src/main/kotlin/io/github/elisatronetti/AlignmentIrGenerationExtension.kt
+++ plugin/compiler-plugin/src/main/kotlin/io/github/elisatronetti/AlignmentIrGenerationExtension.kt
@@ -1,12 +1,10 @@
 package io.github.elisatronetti
 
-import io.github.elisatronetti.visitors.collectAggregateContextClass
-import io.github.elisatronetti.visitors.collectAlignedOnFunction
+import io.github.elisatronetti.utils.common.Name
 import org.jetbrains.kotlin.backend.common.extensions.IrGenerationExtension
 import org.jetbrains.kotlin.backend.common.extensions.IrPluginContext
-import org.jetbrains.kotlin.ir.declarations.IrClass
-import org.jetbrains.kotlin.ir.declarations.IrFunction
 import org.jetbrains.kotlin.ir.declarations.IrModuleFragment
+import org.jetbrains.kotlin.name.FqName
 
 /**
  * The generation extension is used to register the transformer plugin, which is going to modify
@@ -15,15 +13,15 @@ import org.jetbrains.kotlin.ir.declarations.IrModuleFragment
 class AlignmentIrGenerationExtension: IrGenerationExtension {
     override fun generate(moduleFragment: IrModuleFragment, pluginContext: IrPluginContext) {
         // Function that is responsible to handle the alignment
-        val alignedOnFunction: IrFunction? = collectAlignedOnFunction(moduleFragment)
+        val alignedOnFunction = pluginContext.referenceFunctions(FqName(Name.ALIGNED_ON_FUNCTION_FQ_NAME)).firstOrNull()
         // Aggregate Context class that has the reference to the stack
-        val aggregateContextClass: IrClass? = collectAggregateContextClass(moduleFragment)
-        if (alignedOnFunction != null && aggregateContextClass != null) {
+        val aggregateContextClass = pluginContext.referenceClass(FqName(Name.AGGREGATE_CONTEXT_CLASS))
+        if (alignedOnFunction!= null && aggregateContextClass != null) {
             moduleFragment.transform(
                 AggregateCallTransformer(
                     pluginContext,
-                    aggregateContextClass,
-                    alignedOnFunction
+                    aggregateContextClass.owner,
+                    alignedOnFunction.owner
                 ),
                 null
             )
--- plugin/compiler-plugin/src/main/kotlin/io/github/elisatronetti/utils/common/Name.kt
+++ plugin/compiler-plugin/src/main/kotlin/io/github/elisatronetti/utils/common/Name.kt
@@ -3,5 +3,6 @@ package io.github.elisatronetti.utils.common
 object Name {
     const val ALIGNED_ON_FUNCTION = "alignedOn"
     const val AGGREGATE_FUNCTION = "aggregate"
+    const val ALIGNED_ON_FUNCTION_FQ_NAME = "AggregateContext.alignedOn"
     const val AGGREGATE_CONTEXT_CLASS = "AggregateContext"
 }
