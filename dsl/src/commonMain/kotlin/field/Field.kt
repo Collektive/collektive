4e29122bc3b081b5d22fb13f18d7b271ce972324
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2023-02-22 15:43:23 +0100

    feat(field): added plus operator

--- dsl/src/commonMain/kotlin/field/Field.kt
+++ dsl/src/commonMain/kotlin/field/Field.kt
@@ -2,17 +2,24 @@ package field
 
 import ID
 
-interface Field<out T> {
+interface Field<T> {
     val local: Pair<ID, *>
     fun toMap(): Map<ID, T>
     operator fun get(id: ID): T
+    operator fun plus(field: Field<T>): Field<T>
 }
 
 @Suppress("UNCHECKED_CAST")
-class FieldImpl<out T>(override val local: Pair<ID, *>, messages: Map<ID, *> = emptyMap<ID, T>()) : Field<T> {
+class FieldImpl<T>(override val local: Pair<ID, *>, messages: Map<ID, *> = emptyMap<ID, T>()) : Field<T> {
     private val field: Map<ID, T> =  (mapOf(local) + messages) as Map<ID, T>
     override fun get(id: ID): T = field[id]
         ?: throw IllegalArgumentException("No value found for the specified id")
+
+    override fun plus(field: Field<T>): Field<T> {
+        //TODO implement sum between fields
+        return field
+    }
+
     override fun toMap(): Map<ID, T> = field.toMap()
     override fun toString(): String {
         return "FieldImpl(field=$field)"
--- dsl/src/commonMain/kotlin/field/FieldManipulation.kt
+++ dsl/src/commonMain/kotlin/field/FieldManipulation.kt
@@ -3,12 +3,12 @@ package field
 import ID
 
 @Suppress("UNCHECKED_CAST")
-fun <T> Field<Comparable<T>>.min(includingSelf: Boolean = true): T =
+fun <T : Comparable<T>> Field<T>.min(includingSelf: Boolean = true): T =
     handleIncludingSelf(includingSelf).values.reduce { x,y -> if (x <= y as T) x else y } as T
 
 @Suppress("UNCHECKED_CAST")
-fun <T> Field<Comparable<T>>.max(includingSelf: Boolean = true): T =
+fun <T : Comparable<T>> Field<T>.max(includingSelf: Boolean = true): T =
     handleIncludingSelf(includingSelf).values.reduce { x,y -> if (x >= y as T) x else y } as T
 
-private fun <T> Field<Comparable<T>>.handleIncludingSelf(includingSelf: Boolean): Map<ID, Comparable<T>> =
+private fun <T : Comparable<T>> Field<T>.handleIncludingSelf(includingSelf: Boolean): Map<ID, Comparable<T>> =
     this.toMap().filterKeys { id -> (!includingSelf && id != local.first) || includingSelf}
