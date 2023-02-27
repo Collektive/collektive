ccf1dfb572cfd7bb51df6aa925658d3b59347bae
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2023-02-27 17:07:30 +0100

    feat(collektive-test): added gradient extension function to aggregate

--- collektive-test/src/main/kotlin/Aggregate.kt
+++ collektive-test/src/main/kotlin/Aggregate.kt
@@ -1,6 +1,17 @@
-object Aggregate {
-    @JvmStatic
+import field.min
+import field.plus
+import it.unibo.alchemist.model.DistanceSensor
+import it.unibo.alchemist.model.api.CollektiveDevice
+
+class Aggregate(private val node: CollektiveDevice<*>) {
     fun entrypoint() = aggregate {
-        neighbouring("test")
+        gradient(node.node.id == 0, node)
+    }
+}
+
+fun AggregateContext.gradient(source: Boolean, sensor: DistanceSensor) {
+    sharing (Double.POSITIVE_INFINITY) { distance ->
+        val newDistance = distance + sensor.distances()
+        if (source) 0 else newDistance.min(includingSelf = false)
     }
 }
