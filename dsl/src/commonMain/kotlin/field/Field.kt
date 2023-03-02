42e44f01558cc1b1f9fe5b80db61cd066acb622a
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2023-03-02 21:22:37 +0100

    feat(field): added neighbour messages to field as property

--- dsl/src/commonMain/kotlin/field/Field.kt
+++ dsl/src/commonMain/kotlin/field/Field.kt
@@ -2,14 +2,15 @@ package field
 
 import ID
 
-interface Field<T> {
+interface Field<out T> {
     val local: Pair<ID, *>
+    val messages: Map<ID, *>
     fun toMap(): Map<ID, T>
     operator fun get(id: ID): T
 }
 
 @Suppress("UNCHECKED_CAST")
-class FieldImpl<T>(override val local: Pair<ID, *>, messages: Map<ID, *> = emptyMap<ID, T>()) : Field<T> {
+class FieldImpl<T>(override val local: Pair<ID, *>, override val messages: Map<ID, *> = emptyMap<ID, T>()) : Field<T> {
     private val field: Map<ID, T> =  (mapOf(local) + messages) as Map<ID, T>
     override fun get(id: ID): T = field[id]
         ?: throw IllegalArgumentException("No value found for the specified id")
