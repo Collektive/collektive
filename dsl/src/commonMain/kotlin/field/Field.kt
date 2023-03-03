93d55904286ef08da2d743e55e3e0ca81e6dc729
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2023-03-03 18:51:12 +0100

    feat(field): changed field constructor

--- dsl/src/commonMain/kotlin/field/Field.kt
+++ dsl/src/commonMain/kotlin/field/Field.kt
@@ -3,19 +3,26 @@ package field
 import ID
 
 interface Field<out T> {
-    val local: Pair<ID, *>
+    val localId: ID
     val messages: Map<ID, *>
     fun toMap(): Map<ID, T>
+    fun getSelf(): Map<ID, T>
+    fun excludeSelf(): Map<ID, T>
     operator fun get(id: ID): T
 }
 
 @Suppress("UNCHECKED_CAST")
-class FieldImpl<T>(override val local: Pair<ID, *>, override val messages: Map<ID, *> = emptyMap<ID, T>()) : Field<T> {
-    private val field: Map<ID, T> =  (mapOf(local) + messages) as Map<ID, T>
+class FieldImpl<T>(override val localId: ID, override val messages: Map<ID, *> = emptyMap<ID, T>()) : Field<T> {
+
+    private val field: Map<ID, T> = messages as Map<ID, T>
     override fun get(id: ID): T = field[id]
         ?: throw IllegalArgumentException("No value found for the specified id")
 
     override fun toMap(): Map<ID, T> = field.toMap()
+    override fun getSelf(): Map<ID, T> = messages.filterKeys { it == localId } as Map<ID, T>
+
+    override fun excludeSelf(): Map<ID, T> = messages.filterKeys { it != localId } as Map<ID, T>
+
     override fun toString(): String {
         return "FieldImpl(field=$field)"
     }
--- dsl/src/commonMain/kotlin/field/FieldManipulation.kt
+++ dsl/src/commonMain/kotlin/field/FieldManipulation.kt
@@ -11,7 +11,7 @@ fun <T : Comparable<T>> Field<T>.max(includingSelf: Boolean = true): T =
     handleIncludingSelf(includingSelf).values.reduce { x,y -> if (x >= y as T) x else y } as T
 
 private fun <T : Comparable<T>> Field<T>.handleIncludingSelf(includingSelf: Boolean): Map<ID, Comparable<T>> =
-    this.toMap().filterKeys { id -> (!includingSelf && id != local.first) || includingSelf}
+    if (includingSelf) this.toMap() else this.excludeSelf()
 
 operator fun Field<Double>.plus(field: Field<Double>): Field<Double> {
     this.toMap().map { (k,v) -> v + (field.toMap()[k] ?: 0.0) }
