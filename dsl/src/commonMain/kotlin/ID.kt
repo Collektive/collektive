d244f7bbf349d87ce296c6aaeb06bb3f6e08c412
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2023-03-04 16:20:52 +0100

    feat(id): changed id type in int

--- dsl/src/commonMain/kotlin/ID.kt
+++ dsl/src/commonMain/kotlin/ID.kt
@@ -1,5 +1,4 @@
 import kotlin.random.Random
 
 interface ID
-
-data class IntId(val id: Any? = Random.nextInt(Int.MIN_VALUE, Int.MAX_VALUE)) : ID
+data class IntId(val id: Int = Random.nextInt(Int.MIN_VALUE, Int.MAX_VALUE)) : ID
