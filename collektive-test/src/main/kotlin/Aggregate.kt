e9afb4583428111d2bd17b9d382121362efeb42c
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2023-03-04 16:27:39 +0100

    feat(collektive-test): added gradient algorithm

--- collektive-test/src/main/kotlin/Aggregate.kt
+++ collektive-test/src/main/kotlin/Aggregate.kt
@@ -1,17 +1,27 @@
-import field.min
-import field.plus
+import field.*
 import it.unibo.alchemist.model.DistanceSensor
 import it.unibo.alchemist.model.api.CollektiveDevice
+import stack.Path
 
 class Aggregate(private val node: CollektiveDevice<*>) {
-    fun entrypoint() = aggregate {
-        gradient(node.node.id == 0, node)
+    private val nodeId = node.node.id
+    fun entrypoint() = aggregate(
+        IntId(nodeId),
+        node.receive(),
+        emptyMap<Path, Any>()
+    ) {
+        gradient(nodeId == 0, node)
     }
 }
 
-fun AggregateContext.gradient(source: Boolean, sensor: DistanceSensor) {
-    sharing (Double.POSITIVE_INFINITY) { distance ->
-        val newDistance = distance + sensor.distances()
-        if (source) 0 else newDistance.min(includingSelf = false)
+fun AggregateContext.gradient(source: Boolean, sensor: DistanceSensor) =
+    sharing(Double.POSITIVE_INFINITY) { data ->
+        println(data)
+        val paths: Field<Double> = sensor.distances() + data
+        val others: Map.Entry<ID, Double>? = paths.min(includingSelf = false) // field to map, excluding local
+        when {
+            source -> 0.0
+            others == null -> Double.POSITIVE_INFINITY
+            else -> others.value
+        }
     }
-}
