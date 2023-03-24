14e5d3b356389dc681bba4ae531d1052080f1a36
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2023-03-24 11:48:01 +0100

    feat(dsl): remove useless comments

--- dsl/src/commonMain/kotlin/util/Util.kt
+++
@@ -1,8 +0,0 @@
-package util
-
-fun <X,Y,Z> Map<X,Map<Y,Z>>.switchIndexes(): Map<Y,Map<X,Z>> =
-    this.flatMap { (id1, map2) ->
-            map2.toList().map { (id2, value) -> Triple(id2, id1, value) }
-        } // List of triples
-        .groupBy { it.first } // Map<id2, List<Triple>
-        .mapValues { (_, triples) -> triples.associate { it.second to it.third } }
