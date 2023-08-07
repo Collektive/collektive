3f716a766f3c7c28de28dc3bc5fed90019618405
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2023-08-07 13:39:31 +0200

    chore: disable collektive-test project. It should be moved on a separate repository

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,35 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
+
+plugins {
+    id("com.gradle.enterprise") version "3.14"
+    id("org.danilopianini.gradle-pre-commit-git-hooks") version "1.1.9"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.6.0"
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+        publishOnFailure()
+    }
+}
+
+gitHooks {
+    commitMsg { conventionalCommits() }
+    preCommit {
+        tasks("detektAll")
+    }
+    createHooks()
+}
+
 rootProject.name = "collektive"
 
 includeBuild("plugin")
 include("dsl")
-include("collektive-test")
+//include("collektive-test")
