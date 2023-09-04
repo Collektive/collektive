41c86ab243ddf95db39d97a5f83c7edcdefa46a1
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2023-09-04 14:46:00 +0200

    refactor: abstract stack into an interface providing a default implementation based on ArrayDequeue

--- dsl/src/commonMain/kotlin/it/unibo/collektive/stack/Stack.kt
+++ dsl/src/commonMain/kotlin/it/unibo/collektive/stack/Stack.kt
@@ -1,18 +1,29 @@
 package it.unibo.collektive.stack
 
-class Stack<X> {
-    private val currentStack: MutableList<X?> = mutableListOf()
-    fun currentPath(): Path = Path(currentStack.toList())
-    fun alignRaw(token: X?){
-        currentStack.add(token)
+interface Stack<X> {
+    fun currentPath(): Path
+    fun alignRaw(token: X?)
+    fun dealign()
+
+    companion object {
+        operator fun <X> invoke(): Stack<X> = StackImplDequeue()
     }
-    fun dealign(){
+}
+
+internal class StackImplDequeue<X> : Stack<X> {
+    private val currentStack = ArrayDeque<X?>()
+
+    override fun currentPath(): Path = Path(currentStack.toList())
+
+    override fun alignRaw(token: X?) {
+        currentStack.addLast(token)
+    }
+
+    override fun dealign() {
         currentStack.removeLast()
     }
 
-    override fun toString(): String {
-        return currentStack.toString()
-    }
+    override fun toString(): String = currentStack.toString()
 }
 
 data class Path(val path: List<Any?>)
