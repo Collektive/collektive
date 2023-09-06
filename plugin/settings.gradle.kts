f59e4f601e6f28f63e7ed732be0ee2ba56cedcd3
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2023-09-06 16:54:38 +0200

    chore(build): use a single catalog source for all submodules

--- plugin/settings.gradle.kts
+++ plugin/settings.gradle.kts
@@ -1,3 +1,10 @@
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libs") {
+            from(files("../gradle/libs.versions.toml"))
+        }
+    }
+}
+
 include(":compiler-plugin")
 include(":gradle-plugin")
-include(":dsl")
