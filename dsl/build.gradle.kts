52a6688b1cf79b24e591c136d6a3c22c8d1fe036
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2023-09-14 17:08:11 +0200

    chore: use new dsl entrypoint name

--- dsl/build.gradle.kts
+++ dsl/build.gradle.kts
@@ -113,14 +113,14 @@ kotlin {
     }
 }
 
-kotlinAlignmentPlugin {
+// The following configuration is redundant since the gradle plugin enables the compiler plugin by default
+// It is used to configure the compiler plugin
+collektive {
     enabled = true
 }
 
 afterEvaluate {
-    listOf(
-        "uploadKotlinOSSRHToMavenCentralNexus",
-    ).forEach {
+    listOf("uploadKotlinOSSRHToMavenCentralNexus").forEach {
         tasks.named(it).configure {
             enabled = false
         }
