77bbdf3bbf23842fa099c2c77997d76b8810c4f5
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2023-09-27 17:22:19 +0200

    test: add regression test for issue #51

---
+++ dsl/src/commonTest/kotlin/it/unibo/collektive/alignment/TestAlignment.kt
@@ -0,0 +1,31 @@
+package it.unibo.collektive.alignment
+
+import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.collections.shouldContainAll
+import io.kotest.matchers.shouldBe
+import it.unibo.collektive.aggregate
+import it.unibo.collektive.stack.Path
+
+class TestAlignment : FreeSpec({
+    "The alignment" - {
+        "should be performed consistently also for the same aggregate operation called multiple times (issue #51)" {
+            val result = aggregate {
+                neighbouring(10) // path -> [neighbouring.1] = 10
+                sharing(0) {
+                    neighbouring(20) // path -> [sharing.1, neighbouring.2] = 20
+                } // path -> [sharing.1] = Field(...)
+                neighbouring(30) // path -> [neighbouring.3] = 30
+                5
+            }
+
+            result.result shouldBe 5
+            result.toSend.keys.size shouldBe 4 // 4 paths of alignment
+            result.toSend.keys shouldContainAll setOf(
+                Path(listOf("neighbouring.1")),
+                Path(listOf("sharing.1", "neighbouring.2")),
+                Path(listOf("sharing.1")),
+                Path(listOf("neighbouring.3")),
+            )
+        }
+    }
+})
