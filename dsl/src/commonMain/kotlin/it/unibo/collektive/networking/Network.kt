a49bc91b4607b343f1c69bb60b8fa1db641cf27a
Angela Cortecchia <angela.cortecchia@hotmail.com>
2023-10-18 09:46:44 +0200

    feat: change functioning of Network

--- dsl/src/commonMain/kotlin/it/unibo/collektive/networking/Network.kt
+++ dsl/src/commonMain/kotlin/it/unibo/collektive/networking/Network.kt
@@ -12,13 +12,13 @@ interface Network {
      * @param localId id of the device.
      * @param message message to send.
      */
-    fun push(messages: List<Message>)
+    fun send(messages: List<Message>)
 
     /**
      * Receive the messages sent by the other nodes.
      * @return a map with the messages sent by the other nodes.
      */
-    fun fetch(recipientID: ID): Map<ID, Map<Path, *>>
+    fun receive(recipientID: ID): Map<ID, Map<Path, *>>
 
     /**
      * Add a device to the network's list of devices.
@@ -37,28 +37,4 @@ interface Network {
      * @return list of ID.
      */
     fun getDevices(): List<ID>
-}
-
-/**
- * Implementation of the [Network] interface.
- * @param protocolManager: protocol manager used to send and receive messages.
- */
-class NetworkImpl(private val protocolManager: ProtocolManager) : Network {
-    private var connectedDevices = mutableListOf<ID>()
-
-    override fun push(messages: List<Message>) {
-        messages.forEach { protocolManager.sendMessage(it) }
-    }
-
-    override fun fetch(recipientID: ID): Map<ID, Map<Path, *>> = protocolManager.receiveMessage(recipientID)
-
-    override fun connectDevice(id: ID) {
-        connectedDevices.add(id)
-    }
-
-    override fun disconnectDevice(id: ID) {
-        connectedDevices.remove(id)
-    }
-
-    override fun getDevices(): List<ID> = connectedDevices.toList()
 }
