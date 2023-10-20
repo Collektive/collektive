05ce1b5fc0d3123acc9a87b26495de219c345283
Angela Cortecchia <angela.cortecchia@hotmail.com>
2023-10-20 22:10:49 +0200

    feat: update method's signatures

--- dsl/src/commonMain/kotlin/it/unibo/collektive/networking/Network.kt
+++ dsl/src/commonMain/kotlin/it/unibo/collektive/networking/Network.kt
@@ -1,7 +1,7 @@
 package it.unibo.collektive.networking
 
-import it.unibo.collektive.ID
-import it.unibo.collektive.stack.Path
+import it.unibo.collektive.messages.ReceivedMessage
+import it.unibo.collektive.messages.SentMessage
 
 /**
  * Network interface for the aggregate computation.
@@ -9,32 +9,13 @@ import it.unibo.collektive.stack.Path
 interface Network {
     /**
      * Sends a message to the other nodes.
-     * @param localId id of the device.
-     * @param message message to send.
+     * @param messages: the set of messages to be sent.
      */
-    fun send(messages: List<Message>)
+    fun write(messages: Set<SentMessage>)
 
     /**
      * Receive the messages sent by the other nodes.
-     * @return a map with the messages sent by the other nodes.
+     * @return the set of messages received.
      */
-    fun receive(recipientID: ID): Map<ID, Map<Path, *>>
-
-    /**
-     * Add a device to the network's list of devices.
-     * @param id: id of the device.
-     */
-    fun connectDevice(id: ID)
-
-    /**
-     * Remove a device from the network's list of devices.
-     * @param id: id of the device.
-     */
-    fun disconnectDevice(id: ID)
-
-    /**
-     * Get the list of devices connected to the network.
-     * @return list of ID.
-     */
-    fun getDevices(): List<ID>
+    fun read(): Set<ReceivedMessage>
 }
