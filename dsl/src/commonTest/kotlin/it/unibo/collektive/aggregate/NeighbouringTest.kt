dca4622027cbc7fa48c8ad4034bd5c94300c2eb6
Angela Cortecchia <angela.cortecchia@hotmail.com>
2023-10-26 17:08:13 +0200

    test: update neighbouring test with kotest

--- dsl/src/commonTest/kotlin/it/unibo/collektive/aggregate/NeighbouringTest.kt
+++ dsl/src/commonTest/kotlin/it/unibo/collektive/aggregate/NeighbouringTest.kt
@@ -39,7 +39,6 @@ class NeighbouringTest : StringSpec({
         val testNetwork1 = NetworkImplTest(nm, id1)
         aggregate(id1, condition, testNetwork1) {
             val field = neighbouring(double(initV1))
-            println("field 1 $field")
             field.toMap() shouldContainValue 2
         }
 
@@ -48,7 +47,6 @@ class NeighbouringTest : StringSpec({
         val testNetwork2 = NetworkImplTest(nm, id2)
         aggregate(id2, condition, testNetwork2) {
             val field = neighbouring(double(initV2))
-            println("field 2 $field")
             field.toMap() shouldContainValue 2
             field.toMap() shouldContainValue 4
         }
@@ -58,7 +56,6 @@ class NeighbouringTest : StringSpec({
         val testNetwork3 = NetworkImplTest(nm, id3)
         aggregate(id3, condition, testNetwork3) {
             val field = neighbouring(double(initV3))
-            println("field 3 $field")
             field.toMap() shouldContainValue 2
             field.toMap() shouldContainValue 4
             field.toMap() shouldContainValue 6
@@ -77,7 +74,6 @@ class NeighbouringTest : StringSpec({
             fun kingBehaviour() = neighbouring(double(initV2))
             fun queenBehaviour() = neighbouring(add(initV1))
             val f = if (isDeviceOneKing) kingBehaviour() else queenBehaviour()
-            println("f $f")
             f.toMap() shouldHaveSize 1
         }
 
@@ -89,7 +85,6 @@ class NeighbouringTest : StringSpec({
             fun kingBehaviour() = neighbouring(double(initV1))
             fun queenBehaviour() = neighbouring(add(initV2))
             val field = if (isDeviceTwoKing) kingBehaviour() else queenBehaviour()
-            println("field $field")
             field.toMap() shouldHaveSize 1
         }
     }
