aa53c3b3ccc4c81d5e0dc84f155e7cdb4bfd5c95
Angela Cortecchia <angela.cortecchia@hotmail.com>
2023-10-27 10:13:06 +0200

    feat: move sharing context out of Aggregate ops

---
+++ dsl/src/commonMain/kotlin/it/unibo/collektive/SharingContext.kt
@@ -0,0 +1,29 @@
+package it.unibo.collektive
+
+/**
+ * The lambda context passed to the [share] function.
+ */
+class SharingContext<Initial, Return> {
+    internal var toBeSent: Initial? = null
+    internal var toBeReturned: Return? = null
+    internal var areSameType = true
+
+    /**
+     * Express the value [toReturn] after the [share] computation is done.
+     * It can be used with checks after the invocation.
+     *
+     * ## Example
+     * ```
+     * val res3 = share(initV1) {
+     *   val min = it.min()?.value!!
+     *   min butReturn if (min > 1) "Hello" else null
+     * }
+     * ```
+     */
+    infix fun Initial.butReturn(toReturn: Return): Return {
+        toBeSent = this
+        toBeReturned = toReturn
+        areSameType = false
+        return toReturn
+    }
+}
