58a9e53897049ab96bf5868d33446cd8c75e20a1
Angela Cortecchia <angela.cortecchia@hotmail.com>
2023-10-27 15:31:20 +0200

    refactor: uniform generics

--- dsl/src/commonMain/kotlin/it/unibo/collektive/aggregate/AggregateResult.kt
+++ dsl/src/commonMain/kotlin/it/unibo/collektive/aggregate/AggregateResult.kt
@@ -9,9 +9,9 @@ import it.unibo.collektive.state.State
  * It represents the [localId] of the device, the [result] of the computation,
  * the messages [toSend] to other devices and the [newState] of the device.
  */
-data class AggregateResult<X>(
+data class AggregateResult<R>(
     val localId: ID,
-    val result: X,
+    val result: R,
     val toSend: Set<OutboundMessage>,
     val newState: Set<State<*>>,
 )
