dab1a75cbd47ec5e9395a3795126a45093f0c74f
Angela Cortecchia <angela.cortecchia@hotmail.com>
2023-10-27 12:07:55 +0200

    test: update if else single expression test

--- dsl/src/commonTest/kotlin/it/unibo/collektive/branch/IfElseSingleExpressionTest.kt
+++ dsl/src/commonTest/kotlin/it/unibo/collektive/branch/IfElseSingleExpressionTest.kt
@@ -1,36 +1,33 @@
 package it.unibo.collektive.branch
 
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.collections.shouldContain
+import it.unibo.collektive.IntId
 import it.unibo.collektive.aggregate
-import kotlin.test.Test
-import kotlin.test.assertTrue
+import it.unibo.collektive.messages.getPaths
+import it.unibo.collektive.neighbouring
+import it.unibo.collektive.stack.Path
 
-class IfElseSingleExpressionTest {
+class IfElseSingleExpressionTest : StringSpec({
+    val id0 = IntId(0)
 
-    @Test
-    fun trueConditionIfElseBlock() {
+    "True condition in if else block" {
         val customCondition = true
-        val result = aggregate {
+        val result = aggregate(id0) {
             if (customCondition) neighbouring("test") else neighbouring("test")
         }
-        assertTrue(
-            result.toSend.keys.any {
-                it.path.toString().contains("customCondition") &&
-                    it.path.toString().contains("true")
-            },
-        )
+        var paths = emptySet<Path>()
+        result.toSend.forEach { paths = paths + it.getPaths() }
+        paths shouldContain Path(listOf("branch[customCondition, true]", "neighbouring.1", "exchange.1"))
     }
 
-    @Test
-    fun falseConditionIfElseBlock() {
+    "False condition in if else block" {
         val customCondition = false
-        val result = aggregate {
+        val result = aggregate(id0) {
             if (customCondition) neighbouring("test") else neighbouring("test")
         }
-        assertTrue(
-            result.toSend.keys.any {
-                it.path.toString().contains("constant") &&
-                    it.path.toString().contains("false")
-            },
-        )
+        var paths = emptySet<Path>()
+        result.toSend.forEach { paths = paths + it.getPaths() }
+        paths shouldContain Path(listOf("branch[constant, false]", "neighbouring.2", "exchange.1"))
     }
-}
+})
