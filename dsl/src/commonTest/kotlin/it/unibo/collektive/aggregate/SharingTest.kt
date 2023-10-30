6550108e271de454019c37cc8148c9f00e3644d1
Angela Cortecchia <angela.cortecchia@hotmail.com>
2023-10-30 17:29:07 +0100

    test: update sharing tests

--- dsl/src/commonTest/kotlin/it/unibo/collektive/aggregate/SharingTest.kt
+++ dsl/src/commonTest/kotlin/it/unibo/collektive/aggregate/SharingTest.kt
@@ -90,11 +90,11 @@ class SharingTest : StringSpec({
 
             val res2 = share(initV1) {
                 val min = it.maxBy { v -> v.value }.value
-                min.yielding("A string")
+                min.yielding { "A string" }
             }
             res2 shouldBe "A string"
 
-            val res3: String? = share(initV1) {
+            val res3 = share(initV1) {
                 val min = it.minBy { v -> v.value }.value
                 min.yielding { if (min > 1) "Hello" else null }
             }
