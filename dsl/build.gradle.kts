5d045520855867427f11795f8dff5a79859bcbf9
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2023-11-03 17:47:56 +0100

    build: add arrow dependency

--- dsl/build.gradle.kts
+++ dsl/build.gradle.kts
@@ -2,6 +2,16 @@ plugins {
     id("it.unibo.collektive.collektive-plugin")
 }
 
+kotlin {
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(rootProject.libs.arrow)
+            }
+        }
+    }
+}
+
 // The following configuration is redundant since the gradle plugin enables the compiler plugin by default
 // It is used to configure the compiler plugin
 collektive {
