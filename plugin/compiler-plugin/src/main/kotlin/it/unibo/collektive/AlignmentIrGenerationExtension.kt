4f9507b3d2bea8ff574a8efb50a7a6627941deb7
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2023-11-04 10:58:27 +0100

    fix: get alignOn function direclty from the AggregateContext class

--- plugin/compiler-plugin/src/main/kotlin/it/unibo/collektive/AlignmentIrGenerationExtension.kt
+++ plugin/compiler-plugin/src/main/kotlin/it/unibo/collektive/AlignmentIrGenerationExtension.kt
@@ -6,7 +6,7 @@ import org.jetbrains.kotlin.backend.common.extensions.IrGenerationExtension
 import org.jetbrains.kotlin.backend.common.extensions.IrPluginContext
 import org.jetbrains.kotlin.cli.common.messages.MessageCollector
 import org.jetbrains.kotlin.ir.declarations.IrModuleFragment
-import org.jetbrains.kotlin.name.CallableId
+import org.jetbrains.kotlin.ir.util.functions
 import org.jetbrains.kotlin.name.ClassId
 import org.jetbrains.kotlin.name.FqName
 import org.jetbrains.kotlin.name.Name
@@ -17,23 +17,24 @@ import org.jetbrains.kotlin.name.Name
  */
 class AlignmentIrGenerationExtension(private val logger: MessageCollector) : IrGenerationExtension {
     override fun generate(moduleFragment: IrModuleFragment, pluginContext: IrPluginContext) {
-        // Function that is responsible to handle the alignment
-        val alignedOnFunction = pluginContext.referenceFunctions(
-            CallableId(
-                FqName(AggregateFunctionNames.AGGREGATE_CONTEXT_CLASS),
-                Name.identifier(AggregateFunctionNames.ALIGNED_ON_FUNCTION),
-            ),
-        ).firstOrNull()
-
         // Aggregate Context class that has the reference to the stack
         val aggregateContextClass = pluginContext.referenceClass(
             ClassId.topLevel(FqName(AggregateFunctionNames.AGGREGATE_CONTEXT_CLASS)),
         )
 
+        // Function that handles the alignment
+        val alignedOnFunction = aggregateContextClass
+            ?.functions
+            ?.filter { it.owner.name == Name.identifier(AggregateFunctionNames.ALIGNED_ON_FUNCTION) }
+            ?.firstOrNull()
+
         val (alignFunc, aggCtxClass) = getBothOrNull(alignedOnFunction, aggregateContextClass)
             ?: return logger.warn("The function and the class used to handle the alignment have not been found.")
 
-        moduleFragment.transform(AggregateCallTransformer(pluginContext, logger, aggCtxClass.owner, alignFunc.owner), null)
+        moduleFragment.transform(
+            AggregateCallTransformer(pluginContext, logger, aggCtxClass.owner, alignFunc.owner),
+            null,
+        )
     }
 
     private fun <F, S> getBothOrNull(first: F?, second: S?): Pair<F, S>? =
