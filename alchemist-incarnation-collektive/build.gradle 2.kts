dabbbb48c272323aa900f22d1e4e3d80bce6084a
Angela Cortecchia <angela.cortecchia@hotmail.com>
2023-11-17 15:51:13 +0100

    chore: partial cherry with incarnation branch

---
+++ alchemist-incarnation-collektive/build.gradle 2.kts
@@ -0,0 +1,13 @@
+apply(plugin = libs.plugins.kotlin.jvm.id)
+
+kotlinJvm {
+    sourceSets {
+        val main by getting {
+            dependencies {
+                implementation(project(":dsl"))
+                implementation(rootProject.libs.alchemist.api)
+                implementation(rootProject.libs.alchemist)
+            }
+        }
+    }
+}
