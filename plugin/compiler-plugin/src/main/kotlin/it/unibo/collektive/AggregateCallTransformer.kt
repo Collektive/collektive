2f2007d8018eed90e725e95334dc611feef711cf
Angela Cortecchia <angela.cortecchia@hotmail.com>
2023-12-01 12:50:36 +0100

    feat: add invoke inside companion object and other entrypoints

--- plugin/compiler-plugin/src/main/kotlin/it/unibo/collektive/AggregateCallTransformer.kt
+++ plugin/compiler-plugin/src/main/kotlin/it/unibo/collektive/AggregateCallTransformer.kt
@@ -2,6 +2,8 @@ package it.unibo.collektive
 
 import it.unibo.collektive.utils.common.AggregateFunctionNames
 import it.unibo.collektive.utils.common.getLastValueArgument
+import it.unibo.collektive.utils.logging.debug
+import it.unibo.collektive.utils.logging.error
 import org.jetbrains.kotlin.backend.common.extensions.IrPluginContext
 import org.jetbrains.kotlin.cli.common.messages.MessageCollector
 import org.jetbrains.kotlin.ir.IrStatement
@@ -10,6 +12,7 @@ import org.jetbrains.kotlin.ir.declarations.IrFunction
 import org.jetbrains.kotlin.ir.expressions.IrCall
 import org.jetbrains.kotlin.ir.expressions.IrExpression
 import org.jetbrains.kotlin.ir.expressions.IrFunctionExpression
+import org.jetbrains.kotlin.ir.util.dumpKotlinLike
 import org.jetbrains.kotlin.ir.visitors.IrElementTransformerVoid
 
 typealias AlignedData = Map<String, Int>
@@ -27,15 +30,11 @@ class AggregateCallTransformer(
 ) : IrElementTransformerVoid() {
 
     override fun visitFunction(declaration: IrFunction): IrStatement {
-        if (!declaration.name.isSpecial && declaration.extensionReceiverParameter?.type == aggregateContextClass.thisReceiver?.type) {
+        if (!declaration.name.isSpecial &&
+            declaration.extensionReceiverParameter?.type == aggregateContextClass.thisReceiver?.type
+        ) {
             declaration.transformChildren(
-                AlignmentTransformer(
-                    pluginContext,
-                    logger,
-                    aggregateContextClass,
-                    declaration,
-                    alignedOnFunction,
-                ),
+                AlignmentTransformer(pluginContext, logger, aggregateContextClass, declaration, alignedOnFunction),
                 null,
             )
         }
@@ -44,18 +43,25 @@ class AggregateCallTransformer(
 
     override fun visitCall(expression: IrCall): IrExpression {
         if (expression.symbol.owner.name.asString() == AggregateFunctionNames.AGGREGATE_FUNCTION) {
-            val aggregateLambdaBody =
-                (expression.getLastValueArgument() as IrFunctionExpression).function
-            expression.transformChildren(
-                AlignmentTransformer(
-                    pluginContext,
-                    logger,
-                    aggregateContextClass,
-                    aggregateLambdaBody,
-                    alignedOnFunction,
-                ),
-                null,
-            )
+            when (val aggregateLambdaBody = expression.getLastValueArgument()) {
+                is IrFunctionExpression -> expression.transformChildren(
+                    AlignmentTransformer(
+                        pluginContext,
+                        logger,
+                        aggregateContextClass,
+                        aggregateLambdaBody.function,
+                        alignedOnFunction,
+                    ),
+                    null,
+                )
+                is IrCall -> println("TODO")
+                else -> {
+                    logger.debug("Error on visiting call ${expression.symbol.owner.dumpKotlinLike()}")
+                    logger.error(
+                        "Error visiting element of unsupported type '${aggregateLambdaBody!!::class.simpleName}'",
+                    )
+                }
+            }
         }
         return super.visitCall(expression)
     }
