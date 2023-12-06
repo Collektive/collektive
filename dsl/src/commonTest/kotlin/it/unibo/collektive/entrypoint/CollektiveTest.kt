5d1c34714f1cfcab5f784c9a36dfd6506aba53b1
Angela Cortecchia <angela.cortecchia@hotmail.com>
2023-12-06 12:45:01 +0100

    test: add more test for collektive entrypoint

--- dsl/src/commonTest/kotlin/it/unibo/collektive/entrypoint/CollektiveTest.kt
+++ dsl/src/commonTest/kotlin/it/unibo/collektive/entrypoint/CollektiveTest.kt
@@ -1,57 +1,173 @@
 package it.unibo.collektive.entrypoint
 
 import io.kotest.core.spec.style.StringSpec
-import io.kotest.matchers.collections.shouldHaveAtLeastSize
 import io.kotest.matchers.collections.shouldHaveSize
+import io.kotest.matchers.shouldBe
 import it.unibo.collektive.Collektive
 import it.unibo.collektive.IntId
 import it.unibo.collektive.aggregate.AggregateContext
 import it.unibo.collektive.field.Field
+import it.unibo.collektive.messages.InboundMessage
 import it.unibo.collektive.network.NetworkImplTest
 import it.unibo.collektive.network.NetworkManager
+import it.unibo.collektive.stack.Path
 
 class CollektiveTest : StringSpec({
     val id0 = IntId(0)
     val id1 = IntId(1)
 
     val initV1 = 1
-//    val initV2 = 2
+    val initV2 = 2
+    val initV3 = 3
 
     val increaseOrDouble: (Field<Int>) -> Field<Int> = { f ->
         f.mapWithId { _, v -> if (v % 2 == 0) v + 1 else v * 2 }
     }
 
-    val networkManager = NetworkManager()
-    val network0 = NetworkImplTest(networkManager, id0)
-    val network1 = NetworkImplTest(networkManager, id1)
+    val computeFunctionDevice0: AggregateContext.() -> Int = {
+        exchange(initV2, increaseOrDouble).localValue
+    }
 
-//    val computeFunction0: AggregateContext.() -> Int = {
-//        exchange(initV1, increaseOrDouble).localValue
-//    }
+    fun AggregateContext.computeFunctionDevice1(): Int = exchange(initV3, increaseOrDouble).localValue
 
-
-//    val computeFunction1: AggregateContext.() -> Int = {
-//        exchange(initV2, increaseOrDouble).localValue
-//    }
-
-    "Collektive should receive and send messages with cycle function" {
-        fun AggregateContext.computeFunction1(): Int = exchange(initV1, increaseOrDouble).localValue
-        val collektiveDevice0 = Collektive(id0, network0) { computeFunction1() }
-        println("net0 ${network0.read()}")
-        val res0 = collektiveDevice0.cycle()
-        println("network for id0 ${network0.read()}")
-        println("res0 $res0")
-        network0.read() shouldHaveSize 0
-
-        val collektiveDevice1 = Collektive(id1, network1) {
+    "One Collektive device with cycle() as entrypoint should work fine" {
+        val networkManager = NetworkManager()
+        val network0 = NetworkImplTest(networkManager, id0)
+        val collectiveDevice = Collektive(id0, network0) {
             exchange(initV1, increaseOrDouble).localValue
         }
-        println("net1 ${network1.read()}")
-        val res1 = collektiveDevice1.cycle()
-        network1.read() shouldHaveSize 1
-        val r = network0.read().first()
-        r.messages.keys.forEach { e -> e.path shouldHaveAtLeastSize 1 }
-        println("network for id1 ${network1.read()}")
-        println("res1 $res1")
+
+        val result = collectiveDevice.cycle()
+        result shouldBe 2
+        network0.read() shouldHaveSize 0
     }
+
+    "One Collektive device with cycleWhile() as entrypoint should return the cycled result" {
+        val networkManager = NetworkManager()
+        val network0 = NetworkImplTest(networkManager, id0)
+        val collectiveDevice = Collektive(id0, network0) {
+            exchange(initV1, increaseOrDouble).localValue
+        }
+
+        val result = collectiveDevice.cycleWhile { it.result < 10 }
+        result shouldBe 14
+        network0.read() shouldHaveSize 0
+    }
+
+    "Collektive compute function can be a val" {
+        val networkManager = NetworkManager()
+        val network0 = NetworkImplTest(networkManager, id0)
+        val collectiveDevice = Collektive(id0, network0, computeFunctionDevice0)
+
+        val result = collectiveDevice.cycle()
+        result shouldBe 3
+        network0.read() shouldHaveSize 0
+    }
+
+    "Collektive compute function can be a function" {
+        val networkManager = NetworkManager()
+        val network0 = NetworkImplTest(networkManager, id0)
+        val collectiveDevice = Collektive(id0, network0) { computeFunctionDevice1() }
+
+        val result = collectiveDevice.cycle()
+        result shouldBe 6
+        network0.read() shouldHaveSize 0
+    }
+
+    "Two Collektive aligned devices with cycle() as entrypoint should exchange messages and results fine" {
+        val networkManager = NetworkManager()
+        val network0 = NetworkImplTest(networkManager, id0)
+        val network1 = NetworkImplTest(networkManager, id1)
+        val path = Path(listOf("invoke.1", "exchange.1"))
+
+        val collektiveDevice0 = Collektive(id0, network0, computeFunctionDevice0)
+        val collektiveDevice1 = Collektive(id1, network1, computeFunctionDevice0)
+
+        collektiveDevice0.cycle() shouldBe 3
+        network0.read() shouldHaveSize 0
+
+        collektiveDevice1.cycle() shouldBe 3
+        network1.read() shouldHaveSize 1
+
+        network1.read() shouldBe listOf(
+            InboundMessage(id0, mapOf(path to 3)),
+        )
+        network0.read() shouldBe listOf(
+            InboundMessage(id1, mapOf(path to 6)),
+        )
+    }
+
+    "Two Collektive aligned devices with cycleWhile() as entrypoint should exchange messages and results fine" {
+        val networkManager = NetworkManager()
+        val network0 = NetworkImplTest(networkManager, id0)
+        val network1 = NetworkImplTest(networkManager, id1)
+        val path = Path(listOf("invoke.1", "exchange.1"))
+
+        val collektiveDevice0 = Collektive(id0, network0, computeFunctionDevice0)
+        val collektiveDevice1 = Collektive(id1, network1, computeFunctionDevice0)
+
+        collektiveDevice0.cycleWhile { it.result < 6 } shouldBe 6
+        network0.read() shouldHaveSize 0
+        network1.read() shouldBe listOf(
+            InboundMessage(id0, mapOf(path to 3)),
+            InboundMessage(id0, mapOf(path to 6)),
+        )
+
+        collektiveDevice1.cycleWhile { it.result < 10 } shouldBe 14
+        network1.read() shouldHaveSize 2
+        network0.read() shouldHaveSize 4
+
+        // todo I'm not sure it's right
+        network0.read() shouldBe listOf(
+            InboundMessage(id1, mapOf(path to 7)),
+            InboundMessage(id1, mapOf(path to 7)),
+            InboundMessage(id1, mapOf(path to 7)),
+            InboundMessage(id1, mapOf(path to 7)),
+        )
+//        network0.read() shouldBe listOf(
+//            InboundMessage(id1, mapOf(path to 3)),
+//            InboundMessage(id1, mapOf(path to 6)),
+//            InboundMessage(id1, mapOf(path to 7)),
+//            InboundMessage(id1, mapOf(path to 14)),
+//        )
+    }
+
+    "Two Collektive aligned devices with cycle() as entrypoint should exchange messages in more than one cycle" {
+        val networkManager = NetworkManager()
+        val network0 = NetworkImplTest(networkManager, id0)
+        val network1 = NetworkImplTest(networkManager, id1)
+        val path = Path(listOf("invoke.1", "exchange.1"))
+
+        val collektiveDevice0 = Collektive(id0, network0) { exchange(1, increaseOrDouble).localValue }
+        val collektiveDevice1 = Collektive(id1, network1) { exchange(2, increaseOrDouble).localValue }
+
+        // from its initial value 1, apply increaseOrDouble, then sends to device1
+        collektiveDevice0.cycle() shouldBe 2
+        network0.read() shouldHaveSize 0
+        network1.read() shouldBe listOf(
+            InboundMessage(id0, mapOf(path to 2)),
+        )
+
+        // from its initial value 2, apply increaseOrDouble, then sends to device0
+        collektiveDevice1.cycle() shouldBe 3
+        network0.read() shouldBe listOf(
+            InboundMessage(id1, mapOf(path to 3)),
+        )
+
+        // from its value after first cycle 2, apply increaseOrDouble, then sends to device1
+        collektiveDevice0.cycle() shouldBe 3
+        network1.read() shouldBe listOf(
+            InboundMessage(id0, mapOf(path to 3)),
+        )
+
+        // from its value after first cycle 3, apply increaseOrDouble, then sends to device1
+        collektiveDevice1.cycle() shouldBe 6
+        network0.read() shouldBe listOf(
+            InboundMessage(id1, mapOf(path to 6)),
+        )
+    }
+
+//    "TODO try with different functions (should be unaligned)" {
+//        TODO()
+//    }
 })
