add359c13269b175afc59b1bed2b29c54fd5dfa3
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2024-01-04 17:07:41 +0100

    test: add regression test for issue #171

--- dsl/src/commonTest/kotlin/it/unibo/collektive/alignment/BranchAlignment.kt
+++ dsl/src/commonTest/kotlin/it/unibo/collektive/alignment/BranchAlignment.kt
@@ -1,12 +1,20 @@
 package it.unibo.collektive.alignment
 
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.collections.shouldContain
 import io.kotest.matchers.collections.shouldHaveSize
+import io.kotest.matchers.shouldBe
 import it.unibo.collektive.Collektive.Companion.aggregate
 import it.unibo.collektive.IntId
 import it.unibo.collektive.aggregate.ops.neighbouring
+import it.unibo.collektive.field.Field.Companion.hood
+import it.unibo.collektive.field.minus
+import it.unibo.collektive.field.plus
+import it.unibo.collektive.network.NetworkImplTest
+import it.unibo.collektive.network.NetworkManager
 import it.unibo.collektive.stack.Path
+import kotlin.random.Random
 
 class BranchAlignment : StringSpec({
     val id0 = IntId(0)
@@ -42,5 +50,24 @@ class BranchAlignment : StringSpec({
             }
         }
         result.toSend.messages.keys shouldHaveSize 0 // 0 path of alignment
+    }
+    "A field should be projected when used in a body of a branch condition (issue #171)" {
+        val nm = NetworkManager()
+        (0..3)
+            .map { IntId(it) }
+            .map { NetworkImplTest(nm, it) to it }
+            .map { (net, id) ->
+                aggregate(id, net) {
+                    val x = neighbouring(0)
+                    x.neighborsCount shouldBe id.id
+                    if (id.id % 2 == 0) {
+                        neighbouring(1).neighborsCount shouldBe id.id / 2
+                        neighbouring(1) + x
+                    } else {
+                        neighbouring(1).neighborsCount shouldBe (id.id - 1) / 2
+                        neighbouring(1) - x
+                    }
+                }
+            }
     }
 })
