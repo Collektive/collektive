b178e318c976ab14980f99870fe304b887a3edf1
Angela Cortecchia <angela.cortecchia@hotmail.com>
2024-01-12 15:17:44 +0100

    chore(deps): add kotlin reflection dependency

--- alchemist-incarnation-collektive/build.gradle.kts
+++ alchemist-incarnation-collektive/build.gradle.kts
@@ -5,9 +5,20 @@ kotlinJvm {
         val main by getting {
             dependencies {
                 implementation(project(":dsl"))
+                implementation(kotlin("reflect"))
+                implementation(libs.bundles.alchemist)
                 implementation(rootProject.libs.alchemist.api)
                 implementation(rootProject.libs.alchemist)
             }
         }
+        val test by getting {
+            dependencies {
+                implementation(rootProject.libs.kotest.runner.junit5.jvm)
+            }
+        }
     }
+}
+
+tasks.withType(org.jetbrains.kotlin.gradle.tasks.KotlinCompile::class).all {
+    kotlinOptions.freeCompilerArgs = listOf("-Xcontext-receivers")
 }
