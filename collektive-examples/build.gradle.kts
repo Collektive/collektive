fe4bc296dd43f47dbd8cf0be327f65d1d3785ab8
Angela Cortecchia <angela.cortecchia@hotmail.com>
2024-01-13 16:37:58 +0100

    chore(deps): add project dependencies

--- collektive-examples/build.gradle.kts
+++ collektive-examples/build.gradle.kts
@@ -7,8 +7,14 @@ kotlinJvm {
                 implementation(project(":dsl"))
                 implementation(rootProject.libs.alchemist.api)
                 implementation(rootProject.libs.alchemist)
+                implementation(rootProject.libs.bundles.alchemist)
                 implementation(project(":alchemist-incarnation-collektive"))
+                implementation(project(":dsl"))
             }
         }
     }
+}
+
+tasks.withType(org.jetbrains.kotlin.gradle.tasks.KotlinCompile::class).all {
+    kotlinOptions.freeCompilerArgs = listOf("-Xcontext-receivers")
 }
