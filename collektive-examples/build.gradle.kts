f64c9a12e409a1024186df85602576d4e3983bd2
Angela Cortecchia <angela.cortecchia@hotmail.com>
2024-01-15 12:30:44 +0100

    feat: add yaml launcher

--- collektive-examples/build.gradle.kts
+++ collektive-examples/build.gradle.kts
@@ -1,5 +1,15 @@
 apply(plugin = libs.plugins.kotlin.jvm.id)
 
+plugins{
+    alias(libs.plugins.multiJvmTesting)
+    alias(libs.plugins.taskTree)
+}
+
+multiJvm {
+    jvmVersionForCompilation.set(17)
+}
+
+
 kotlinJvm {
     sourceSets {
         val main by getting {
@@ -14,6 +24,65 @@ kotlinJvm {
         }
     }
 }
+
+val alchemistGroup = "Run Alchemist"
+
+val runAll by tasks.register<DefaultTask>("runAll") {
+    group = alchemistGroup
+    description = "Launches all simulations"
+}
+
+val batch: String by project
+val maxTime: String by project
+
+/*
+ * Scan the folder with the simulation files, and create a task for each one of them.
+ */
+
+println(rootProject.rootDir.path)
+File(rootProject.rootDir.path + "/collektive-examples/src/main/yaml").listFiles()
+    .orEmpty()
+    .apply { check(isNotEmpty()) }
+    .filter { it.extension == "yml" }
+    .sortedBy { it.nameWithoutExtension }
+    .forEach {
+        val task by tasks.register<JavaExec>("run${it.nameWithoutExtension.uppercase()}") {
+            javaLauncher.set(
+                javaToolchains.launcherFor {
+                    languageVersion.set(JavaLanguageVersion.of(multiJvm.latestJava))
+                }
+            )
+            group = alchemistGroup
+            description = "Launches simulation ${it.nameWithoutExtension}"
+            mainClass.set("it.unibo.alchemist.Alchemist")
+            classpath = sourceSets["main"].runtimeClasspath
+            val exportsDir = File("${projectDir.path}/build/exports/${it.nameWithoutExtension}")
+            doFirst {
+                if (!exportsDir.exists()) {
+                    exportsDir.mkdirs()
+                }
+            }
+            args("run", it.absolutePath)
+            if (System.getenv("CI") == "true" || batch == "true") {
+                args(
+                    "--override",
+                    """
+                    {
+                        launcher: { type: HeadlessSimulationLauncher, parameters: [] },
+                        terminate: { type: AfterTime, parameters: 2 }
+                    }
+                    """.trimIndent()
+                )
+            } else {
+                args(
+                    "--override",
+                    "{ launcher: { parameters: { graphics: \"effects/${it.nameWithoutExtension}.json\" } } }"
+                )
+            }
+            outputs.dir(exportsDir)
+        }
+        runAll.dependsOn(task)
+    }
 
 tasks.withType(org.jetbrains.kotlin.gradle.tasks.KotlinCompile::class).all {
     kotlinOptions.freeCompilerArgs = listOf("-Xcontext-receivers")
