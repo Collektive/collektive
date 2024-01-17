d30214c59c84ad9380534cf3a67f3bc2a3f27e23
Angela Cortecchia <angela.cortecchia@studio.unibo.it>
2024-01-17 12:22:58 +0100

    perf: add override to hashcode to improve performances

--- dsl/src/commonMain/kotlin/it/unibo/collektive/path/impl/PathImpl.kt
+++ dsl/src/commonMain/kotlin/it/unibo/collektive/path/impl/PathImpl.kt
@@ -3,5 +3,13 @@ package it.unibo.collektive.path.impl
 import it.unibo.collektive.path.Path
 
 internal data class PathImpl(private val path: List<Any?>) : Path {
+    private var hash = 0
     override fun tokens(): List<Any?> = path
+
+    override fun hashCode(): Int {
+        if (hash == 0) {
+            hash = path.hashCode()
+        }
+        return hash
+    }
 }
