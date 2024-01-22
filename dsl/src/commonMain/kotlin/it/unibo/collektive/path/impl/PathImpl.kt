5b4db1e08eb7dc5eec3b0cd6bfd464e3a2d8ee1c
Angela Cortecchia <angela.cortecchia@studio.unibo.it>
2024-01-22 12:09:25 +0100

    style: update path hashcode implementation

--- dsl/src/commonMain/kotlin/it/unibo/collektive/path/impl/PathImpl.kt
+++ dsl/src/commonMain/kotlin/it/unibo/collektive/path/impl/PathImpl.kt
@@ -3,13 +3,8 @@ package it.unibo.collektive.path.impl
 import it.unibo.collektive.path.Path
 
 internal data class PathImpl(private val path: List<Any?>) : Path {
-    private var hash = 0
+    private val hash by lazy { path.hashCode() }
     override fun tokens(): List<Any?> = path
 
-    override fun hashCode(): Int {
-        if (hash == 0) {
-            hash = path.hashCode()
-        }
-        return hash
-    }
+    override fun hashCode(): Int = hash
 }
