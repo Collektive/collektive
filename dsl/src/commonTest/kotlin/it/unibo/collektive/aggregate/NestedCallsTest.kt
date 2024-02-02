05c6911ee6401beff55b8f3bf6f7506270ac1c63
Angela Cortecchia <angela.cortecchia@studio.unibo.it>
2024-02-02 18:05:19 +0100

    test: update test

--- dsl/src/commonTest/kotlin/it/unibo/collektive/aggregate/NestedCallsTest.kt
+++ dsl/src/commonTest/kotlin/it/unibo/collektive/aggregate/NestedCallsTest.kt
@@ -3,35 +3,37 @@ package it.unibo.collektive.aggregate
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 import it.unibo.collektive.Collektive
-import it.unibo.collektive.Collektive.Companion.aggregate
 import it.unibo.collektive.aggregate.api.Aggregate
 import it.unibo.collektive.aggregate.api.operators.neighboringViaExchange
 import it.unibo.collektive.aggregate.api.operators.share
+import it.unibo.collektive.field.min
 import it.unibo.collektive.field.plus
 import it.unibo.collektive.network.NetworkImplTest
 import it.unibo.collektive.network.NetworkManager
 
 class NestedCallsTest : StringSpec({
 
-    fun Aggregate<Int>.foo(id: Int) = neighboringViaExchange(id).map { v -> v + 1.0 }
+    fun Aggregate<Int>.foo(id: Int) = neighboringViaExchange(id.toDouble())
 
-    fun Aggregate<Int>.gradient(): Double =
-        share(Double.POSITIVE_INFINITY) { dist ->
+    fun Aggregate<Int>.bar(): Double {
+        return share(Double.POSITIVE_INFINITY) { dist ->
             when (localId) {
                 0 -> 0.0
-                else -> (foo(localId) + dist).excludeSelf().minOfOrNull { it.value } ?: Double.POSITIVE_INFINITY
+                else -> (foo(localId) + dist).min(Double.POSITIVE_INFINITY)
             }
         }
+    }
 
-    "Nested alignment should work fine" {
+    "This is a regression test for the issue #207" {
         val networkManager = NetworkManager()
         val network0 = NetworkImplTest(networkManager, 0)
         val network1 = NetworkImplTest(networkManager, 1)
 
-        val collektiveDevice0 = Collektive(0, network0) { gradient() }
+        val collektiveDevice0 = Collektive(0, network0) { bar() }
         collektiveDevice0.cycle() shouldBe 0
 
-        val collektiveDevice1 = Collektive(1, network1) { gradient() }
-        collektiveDevice1.cycle() shouldBe 2
+        val collektiveDevice1 = Collektive(1, network1) { bar() }
+        println(network1.read())
+        collektiveDevice1.cycle() shouldBe Double.POSITIVE_INFINITY
     }
 })
