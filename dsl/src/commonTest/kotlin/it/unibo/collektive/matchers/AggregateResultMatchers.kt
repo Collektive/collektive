cacd55200b101e8a2608f1186bcec3edc6e5ac78
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2024-02-08 23:08:56 +0100

    test: refactor matcher creation

--- dsl/src/commonTest/kotlin/it/unibo/collektive/matchers/AggregateResultMatchers.kt
+++ dsl/src/commonTest/kotlin/it/unibo/collektive/matchers/AggregateResultMatchers.kt
@@ -5,25 +5,26 @@ import io.kotest.matchers.MatcherResult
 import it.unibo.collektive.Collektive.Companion.aggregate
 import it.unibo.collektive.aggregate.AggregateResult
 import it.unibo.collektive.aggregate.api.Aggregate
+import it.unibo.collektive.path.Path
 
 fun <ID : Any, R> alignWith(expected: AggregateResult<ID, R>) = Matcher<AggregateResult<ID, R>> { valueResult ->
     val originPaths = valueResult.toSend.messages.keys
     val expectedPaths = expected.toSend.messages.keys
-    MatcherResult(
-        originPaths == expectedPaths,
-        { "Expected the following paths: $expectedPaths, but got: $originPaths" },
-        { "Got the following paths: $originPaths, while expecting: $expectedPaths" },
-    )
+    aggregateMatcher(expectedPaths, originPaths)
 }
 
 fun <R> alignWith(expect: Aggregate<Int>.() -> R): Matcher<Aggregate<Int>.() -> R> = Matcher { program ->
     val expected = aggregate(0, emptySet(), emptyMap(), expect).run { toSend.messages.keys }
     val result = aggregate(0, emptySet(), emptyMap(), program).run { toSend.messages.keys }
-    MatcherResult(
-        result == expect,
-        { "Expected the following paths: $expected, but got: $result" },
-        { "Got the following paths: $result, while expecting: $expected" },
-    )
+    aggregateMatcher(expected, result)
 }
 
 fun <R> acProgram(acProgram: Aggregate<Int>.() -> R): Aggregate<Int>.() -> R = acProgram
+
+private fun aggregateMatcher(expected: Set<Path>, actual: Set<Path>): MatcherResult {
+    return MatcherResult(
+        actual == expected,
+        { "Expected the following paths: $expected, but got: $actual" },
+        { "Got the following paths: $actual, while expecting: $expected" },
+    )
+}
