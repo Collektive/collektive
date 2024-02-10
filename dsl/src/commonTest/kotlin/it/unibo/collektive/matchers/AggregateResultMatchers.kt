b670e8558c059f9884be1d7af1ab603ff3cdd7e6
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2024-02-10 10:23:58 +0100

    docs: document collektive matchers

--- dsl/src/commonTest/kotlin/it/unibo/collektive/matchers/AggregateResultMatchers.kt
+++ dsl/src/commonTest/kotlin/it/unibo/collektive/matchers/AggregateResultMatchers.kt
@@ -7,19 +7,52 @@ import it.unibo.collektive.aggregate.AggregateResult
 import it.unibo.collektive.aggregate.api.Aggregate
 import it.unibo.collektive.path.Path
 
+/**
+ * Matcher checking if the result of an aggregate program aligns with the [expected] result.
+ *
+ * The alignment is checked by comparing the paths of the messages in the result with
+ * the paths of the messages in the expected result.
+ *
+ * Example:
+ * ```
+ * val first = aggregate(0) { neighboringViaExchange(10) }
+ * val result = aggregate(0) { neighboringViaExchange(10) }
+ * result should alignWith(first)
+ * ```
+ */
 fun <ID : Any, R> alignWith(expected: AggregateResult<ID, R>) = Matcher<AggregateResult<ID, R>> { valueResult ->
     val originPaths = valueResult.toSend.messages.keys
     val expectedPaths = expected.toSend.messages.keys
     aggregateMatcher(expectedPaths, originPaths)
 }
 
-fun <R> alignWith(expect: Aggregate<Int>.() -> R): Matcher<Aggregate<Int>.() -> R> = Matcher { program ->
-    val expected = aggregate(0, emptySet(), emptyMap(), expect).run { toSend.messages.keys }
+/**
+ * Matcher checking if two aggregate programs align.
+ *
+ * The alignment is checked by comparing the paths of the messages in the result
+ * with the paths of the messages in the [expected] result.
+ *
+ * Example:
+ * ```
+ * acProgram { neighboringViaExchange(0) } should alignWith { neighboringViaExchange(0) }
+ */
+fun <R> alignWith(expected: Aggregate<Int>.() -> R): Matcher<Aggregate<Int>.() -> R> = Matcher { program ->
+    val expectedRes = aggregate(0, emptySet(), emptyMap(), expected).run { toSend.messages.keys }
     val result = aggregate(0, emptySet(), emptyMap(), program).run { toSend.messages.keys }
-    aggregateMatcher(expected, result)
+    aggregateMatcher(expectedRes, result)
 }
 
-fun <R> acProgram(acProgram: Aggregate<Int>.() -> R): Aggregate<Int>.() -> R = acProgram
+/**
+ * Utility test function to create an [aggregateProgram] on fly.
+ *
+ * Can be combined with [alignWith] to check if two aggregate programs align.
+ *
+ * Example:
+ * ```
+ * acProgram { neighboringViaExchange(0) } should alignWith { neighboringViaExchange(0) }
+ * ```
+ */
+fun <R> acProgram(aggregateProgram: Aggregate<Int>.() -> R): Aggregate<Int>.() -> R = aggregateProgram
 
 private fun aggregateMatcher(expected: Set<Path>, actual: Set<Path>): MatcherResult {
     return MatcherResult(
