5a63faa743050e314a3cdfc6ebdabacd8e29325b
Danilo Pianini <danilo.pianini@unibo.it>
2024-02-15 13:23:09 +0100

    test(alchemist-collektive-incarnation): create a testing infrastructure

---
+++ alchemist-incarnation-collektive/src/test/kotlin/it/unibo/collektive/test/TestLoadingWithAlchemist.kt
@@ -0,0 +1,23 @@
+package it.unibo.collektive.test
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.nulls.beNull
+import io.kotest.matchers.shouldNot
+import it.unibo.alchemist.boundary.LoadAlchemist
+import it.unibo.alchemist.model.terminators.AfterTime
+import it.unibo.alchemist.model.times.DoubleTime
+import it.unibo.alchemist.util.ClassPathScanner
+
+class TestLoadingWithAlchemist : StringSpec({
+    ClassPathScanner.resourcesMatching(".*\\.ya?ml", "it.unibo.collektive").forEach { simulationFile ->
+        val (fileName) = checkNotNull(Regex(".*/([^/]+?)$").matchEntire(simulationFile.path)).destructured
+        "test loading and running $fileName" {
+            val loader = LoadAlchemist.from(simulationFile)
+            loader shouldNot beNull()
+            val simulation = loader.getDefault<Any, Nothing>()
+            simulation shouldNot beNull()
+            simulation.environment.addTerminator(AfterTime(DoubleTime(10.0)))
+            loader.launch()
+        }
+    }
+})
