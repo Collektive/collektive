4b95d255be2ac7a9307b546e2a7745472fcfe1cf
Danilo Pianini <danilo.pianini@unibo.it>
2024-02-15 13:16:02 +0100

    fix(compiler-plugin): better deal with the JVM backend IR configuration

--- compiler-plugin/src/main/kotlin/it/unibo/collektive/AlignmentComponentRegistrar.kt
+++ compiler-plugin/src/main/kotlin/it/unibo/collektive/AlignmentComponentRegistrar.kt
@@ -1,11 +1,14 @@
 package it.unibo.collektive
 
+import it.unibo.collektive.utils.logging.info
+import it.unibo.collektive.utils.logging.warn
 import org.jetbrains.kotlin.backend.common.extensions.IrGenerationExtension
 import org.jetbrains.kotlin.cli.common.CLIConfigurationKeys
 import org.jetbrains.kotlin.cli.common.messages.MessageCollector
 import org.jetbrains.kotlin.compiler.plugin.CompilerPluginRegistrar
 import org.jetbrains.kotlin.compiler.plugin.ExperimentalCompilerApi
 import org.jetbrains.kotlin.config.CompilerConfiguration
+import org.jetbrains.kotlin.config.JVMConfigurationKeys
 
 /**
  * The component registrar registers the alignment generation extension if
@@ -18,8 +21,26 @@ class AlignmentComponentRegistrar : CompilerPluginRegistrar() {
 
     override fun ExtensionStorage.registerExtensions(configuration: CompilerConfiguration) {
         val logger = configuration.get(CLIConfigurationKeys.MESSAGE_COLLECTOR_KEY, MessageCollector.NONE)
-        val enabled = configuration.get(AlignmentCommandLineProcessor.ARG_ENABLED) ?: error("No enabled arg")
-        if (enabled) {
+        when (configuration.get(JVMConfigurationKeys.IR)) {
+            false -> error(
+                "The Kotlin-JVM IR backend has been explicitly disabled, but the Collektive compiler plugin requires it"
+            )
+            null -> {
+                when {
+                    configuration.isReadOnly -> logger.warn(
+                        "The Kotlin-JVM IR backend has not been enabled, but the Collektive compiler plugin requires it"
+                    )
+                    else -> configuration.put(JVMConfigurationKeys.IR, true).also {
+                        logger.info(
+                            "Implicitly enabling the Kotlin-JVM IR backend," +
+                                "it is required by the Collektive compiler plugin"
+                        )
+                    }
+                }
+            }
+            else -> Unit
+        }
+        if (configuration.get(AlignmentCommandLineProcessor.ARG_ENABLED) != false) {
             IrGenerationExtension.registerExtension(AlignmentIrGenerationExtension(logger))
         }
     }
