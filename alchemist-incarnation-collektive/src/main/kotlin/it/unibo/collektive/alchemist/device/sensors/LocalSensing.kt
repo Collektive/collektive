67466b98f0b0a9ab62bbf72071b0ae122ce66137
Angela Cortecchia <angela.cortecchia@studio.unibo.it>
2024-02-25 17:56:21 +0100

    feat: add sensing or else to sensors

--- alchemist-incarnation-collektive/src/main/kotlin/it/unibo/collektive/alchemist/device/sensors/LocalSensing.kt
+++ alchemist-incarnation-collektive/src/main/kotlin/it/unibo/collektive/alchemist/device/sensors/LocalSensing.kt
@@ -1,11 +1,16 @@
 package it.unibo.collektive.alchemist.device.sensors
 
 /**
- * Distance sensor.
+ * Interface for a device that can sense the environment.
  */
 interface LocalSensing {
     /**
      * Sense a molecule given its [name] and returns its value.
      */
     fun <T> sense(name: String): T
+
+    /**
+     * Sense a molecule given its [name] and returns its value, or [default] if the molecule is not present.
+     */
+    fun <T> senseOrElse(name: String, default: T): T
 }
