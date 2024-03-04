cb6086e03f1d930c6fec22742ae907d3c91b536d
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2024-03-04 22:34:10 +0100

    chore: use correct task name

--- release.config.js
+++ release.config.js
@@ -1,17 +1,17 @@
 const publishCmd = `
-./gradlew plugin:gradle-plugin:publishPlugins -Pgradle.publish.key=$GRADLE_PUBLISH_KEY -Pgradle.publish.secret=$GRADLE_PUBLISH_SECRET || exit 1
+./gradlew gradle-plugin:publishPlugins -Pgradle.publish.key=$GRADLE_PUBLISH_KEY -Pgradle.publish.secret=$GRADLE_PUBLISH_SECRET || exit 1
 ./gradlew -PstagingRepositoryId=\${process.env.STAGING_REPO_ID} releaseStagingRepositoryOnMavenCentral || exit 2
 `;
 
 const config = require('semantic-release-preconfigured-conventional-commits');
 config.plugins.push(
-  [
-    "@semantic-release/exec",
-    {
-      "publishCmd": publishCmd,
-    }
-  ],
-  "@semantic-release/github",
-  "@semantic-release/git",
+    [
+        "@semantic-release/exec",
+        {
+            "publishCmd": publishCmd,
+        }
+    ],
+    "@semantic-release/github",
+    "@semantic-release/git",
 )
 module.exports = config
