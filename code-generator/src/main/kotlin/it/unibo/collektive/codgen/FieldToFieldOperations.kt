2fd1e4e402836f404151453aebf87bbfe327f7f8
Danilo Pianini <danilo.pianini@unibo.it>
2024-03-06 17:15:00 +0100

    porcaio

--- code-generator/src/main/kotlin/it/unibo/collektive/codgen/FieldToFieldOperations.kt
+++ code-generator/src/main/kotlin/it/unibo/collektive/codgen/FieldToFieldOperations.kt
@@ -11,6 +11,8 @@ import com.squareup.kotlinpoet.TypeVariableName
 import com.squareup.kotlinpoet.asTypeName
 import com.squareup.kotlinpoet.asTypeVariableName
 import com.squareup.kotlinpoet.typeNameOf
+import it.unibo.collektive.field.Field
+import kotlin.coroutines.CoroutineContext
 import kotlin.reflect.KCallable
 import kotlin.reflect.KType
 
@@ -43,20 +45,23 @@ class FieldToFieldOperations : CollektiveCodeGenerator<List<KType>> {
             val functionParameters = origin.parameters.drop(1).map {
                 ParameterSpec(it.name ?: error("No name found"), it.type.asTypeName())
             }
+            // Generate the sub-set of fielded arguments
+            val fieldArguments = origin.parameters.drop(1).map { it.name } // TODO
             val functionSpecification = FunSpec.builder(origin.name).apply {
                 addTypeVariable(ID_BOUNDED_TYPE)
                 addTypeVariables(origin.typeParameters.map { it.asTypeVariableName() })
                 addParameters(functionParameters)
                 receiver(FIELD_INTERFACE.parameterizedBy(ID_BOUNDED_TYPE, origin.parameters[0].type.asTypeName()))
+                returns(origin.returnType.asTypeName())
                 when (functionParameters.size) {
                     0 -> {
-                        addStatement("val result = map { it.${origin.name}() }")
-                        addStatement("return result")
+                        addStatement("return map { it.${origin.name}() }")
                     }
                     1 -> {
-                        val firstArgument = functionParameters[0].name
-                        addStatement("val result = alignedMap($firstArgument) { a, b -> a.${origin.name}(b) }")
-                        addStatement("return result")
+                        addStatement("Field.checkAligned(this, ${fieldArguments.joinToString(separator = ", ") })")
+                        addStatement("return mapWithId { id, receiver -> receiver.${origin.name}(" +
+                                fieldArguments.joinToString(separator = ", ") { "$it[id]" } +
+                                ") })")
                     }
                 }
             }.build()
@@ -69,8 +74,43 @@ class FieldToFieldOperations : CollektiveCodeGenerator<List<KType>> {
  * TODO.
  */
 fun main() {
-    Int::class.members.forEach { println(FieldToFieldOperations.generateFun(it)) }
-
+    val targetTypes = sequenceOf(
+        Boolean::class,
+        Byte::class,
+        Short::class,
+        Int::class,
+        Float::class,
+        Double::class,
+        Long::class,
+        UByte::class,
+        UShort::class,
+        UInt::class,
+        ULong::class,
+        Char::class,
+        CharSequence::class,
+        String::class,
+        Map::class,
+        List::class,
+        Collection::class,
+        Set::class,
+        Array::class,
+        ByteArray::class,
+        ShortArray::class,
+        IntArray::class,
+        FloatArray::class,
+        DoubleArray::class,
+        LongArray::class,
+        Comparator::class,
+        Pair::class,
+        Triple::class,
+        Result::class,
+        Throwable::class,
+    )
+    fun KCallable<*>.paramTypes() = parameters.map { it.type }
+    val forbiddenMembers = Field::class.members.map { member -> member.name to member.paramTypes() }
+    targetTypes.flatMap { it.members }
+        .filterNot { it.name to it.paramTypes() in forbiddenMembers }
+        .forEach { println(FieldToFieldOperations.generateFun(it)) }
     val fieldToFieldOperations = FieldToFieldOperations()
     val fileSpec = fieldToFieldOperations.generate(
         emptyList(),
