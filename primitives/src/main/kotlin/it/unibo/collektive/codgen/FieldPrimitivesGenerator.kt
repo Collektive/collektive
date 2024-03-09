e43e9b3190d28b50f6fe82d66971d65411aeb0d1
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2024-03-09 12:30:02 +0100

    feat: provide a dedicate function for code generation

--- primitives/src/main/kotlin/it/unibo/collektive/codgen/FieldPrimitivesGenerator.kt
+++ primitives/src/main/kotlin/it/unibo/collektive/codgen/FieldPrimitivesGenerator.kt
@@ -1,51 +1,58 @@
 package it.unibo.collektive.codgen
 
+import com.squareup.kotlinpoet.FileSpec
 import it.unibo.collektive.codgen.utils.generatePrimitivesFile
 import it.unibo.collektive.field.Field
-import java.io.File
 import kotlin.reflect.KCallable
+import kotlin.reflect.KClass
 
 /**
- * TODO.
+ * The base types for which to generate field functions.
  */
-fun main() {
-    val targetTypes = sequenceOf(
-        Boolean::class,
-        Byte::class,
-        Short::class,
-        Int::class,
-        Float::class,
-        Double::class,
-        Long::class,
-        UByte::class,
-        UShort::class,
-        UInt::class,
-        ULong::class,
-        Char::class,
-        CharSequence::class,
-        String::class,
-        Map::class,
-        List::class,
-        Collection::class,
-        Set::class,
-        Array::class,
-        ByteArray::class,
-        ShortArray::class,
-        IntArray::class,
-        FloatArray::class,
-        DoubleArray::class,
-        LongArray::class,
-        //        Comparator::class,
-        Pair::class,
-        Triple::class,
-        Result::class,
-        Throwable::class,
-    )
+val baseTargetTypes = sequenceOf(
+    Boolean::class,
+    Byte::class,
+    Short::class,
+    Int::class,
+    Float::class,
+    Double::class,
+    Long::class,
+    UByte::class,
+    UShort::class,
+    UInt::class,
+    ULong::class,
+    Char::class,
+    CharSequence::class,
+    String::class,
+    Map::class,
+    List::class,
+    Collection::class,
+    Set::class,
+    Array::class,
+    ByteArray::class,
+    ShortArray::class,
+    IntArray::class,
+    FloatArray::class,
+    DoubleArray::class,
+    LongArray::class,
+    // Comparator::class,
+    Pair::class,
+    Triple::class,
+    Result::class,
+    Throwable::class,
+)
 
+/**
+ * Generates field-based functions for the given types.
+ */
+fun generateFieldFunctionsForTypes(
+    types: Sequence<KClass<*>>,
+    excludeMembers: List<String> = emptyList(),
+): Sequence<FileSpec> {
     fun KCallable<*>.paramTypes() = parameters.drop(1).map { it.type }
     val forbiddenMembers = Field::class.members.map { member -> member.name to member.paramTypes() }
-    val forbiddenMembersName = listOf("compareTo")
-    val generatedFiles = targetTypes.map { clazz ->
+    val forbiddenMembersName = listOf("compareTo") + excludeMembers
+    return types.map { clazz ->
         val membersToField = clazz.members.filterNot { it.name to it.paramTypes() in forbiddenMembers }
             .filter { it.annotations.isEmpty() }
             .filterNot { it.name in forbiddenMembersName }
@@ -55,9 +62,5 @@ fun main() {
             "it.unibo.collektive.primitives",
             "Field${clazz.simpleName}Primitives",
         )
-    }
-
-    generatedFiles.forEach {
-        it.writeTo(File("primitives/src/main/resources/kotlin"))
     }
 }
