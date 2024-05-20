7a3c422c49ee8e362a6b16e5d86ad0382d3e3fbe
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2024-05-20 21:14:10 +0200

    refactor: rename default into base

--- dsl/src/commonMain/kotlin/it/unibo/collektive/field/operations/Fields.kt
+++ dsl/src/commonMain/kotlin/it/unibo/collektive/field/operations/Fields.kt
@@ -5,32 +5,32 @@ import it.unibo.collektive.field.Field.Companion.fold
 
 /**
  * Count the number of elements in the field that satisfy the [predicate].
- * The [default] value is included in the count if it satisfies the [predicate].
- * If the [default] value is not provided, the [predicate] is applied to the neighbors only.
+ * The [base] value is included in the count if it satisfies the [predicate].
+ * If the [base] value is not provided, the [predicate] is applied to the neighbors only.
  */
-fun <ID : Any, T> Field<ID, T>.count(default: T? = null, predicate: (T) -> Boolean = { true }): Int =
+fun <ID : Any, T> Field<ID, T>.count(base: T? = null, predicate: (T) -> Boolean = { true }): Int =
     when {
-        default == null -> fold(0) { acc, value -> if (predicate(value)) acc + 1 else acc }
-        else -> fold(if (predicate(default)) 1 else 0) { acc, value -> if (predicate(value)) acc + 1 else acc }
+        base == null -> fold(0) { acc, value -> if (predicate(value)) acc + 1 else acc }
+        else -> fold(if (predicate(base)) 1 else 0) { acc, value -> if (predicate(value)) acc + 1 else acc }
     }
 
 /**
  * Check if all the elements in the field satisfy the [predicate].
- * The local value is not considered, unless explicitly passed as [default].
+ * The local value is not considered, unless explicitly passed as [base].
  */
-fun <ID : Any, T> Field<ID, T>.all(default: T, predicate: (T) -> Boolean): Boolean =
-    fold(predicate(default)) { acc, value -> acc && predicate(value) }
+fun <ID : Any, T> Field<ID, T>.all(base: T, predicate: (T) -> Boolean): Boolean =
+    fold(predicate(base)) { acc, value -> acc && predicate(value) }
 
 /**
  * Check if any of the elements in the field satisfy the [predicate].
- * The local value is not considered, unless explicitly passed as [default].
+ * The local value is not considered, unless explicitly passed as [base].
  */
-fun <ID : Any, T> Field<ID, T>.any(default: T, predicate: (T) -> Boolean): Boolean =
-    fold(predicate(default)) { acc, value -> acc || predicate(value) }
+fun <ID : Any, T> Field<ID, T>.any(base: T, predicate: (T) -> Boolean): Boolean =
+    fold(predicate(base)) { acc, value -> acc || predicate(value) }
 
 /**
  * Check if none of the elements in the field satisfy the [predicate].
- * The local value is not considered, unless explicitly passed as [default].
+ * The local value is not considered, unless explicitly passed as [base].
  */
-fun <ID : Any, T> Field<ID, T>.none(default: T, predicate: (T) -> Boolean): Boolean =
-    !all(default, predicate)
+fun <ID : Any, T> Field<ID, T>.none(base: T, predicate: (T) -> Boolean): Boolean =
+    !all(base, predicate)
