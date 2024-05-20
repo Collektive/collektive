e18ee57733efc4afe67ff301a229fca2333dd24e
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2024-05-20 21:14:50 +0200

    refactor: add overload annoation and fix docs

--- dsl/src/commonMain/kotlin/it/unibo/collektive/field/operations/BooleanField.kt
+++ dsl/src/commonMain/kotlin/it/unibo/collektive/field/operations/BooleanField.kt
@@ -2,27 +2,31 @@ package it.unibo.collektive.field.operations
 
 import it.unibo.collektive.field.Field
 import it.unibo.collektive.field.Field.Companion.fold
+import kotlin.jvm.JvmOverloads
 
 /**
  * Check if all the [Boolean] elements in the field are true by default.
  * If [predicate] is defined, it is applied to the elements.
- * The local value is not considered, unless explicitly passed as [default].
+ * The [base] value is used as a first element to start the fold operation at which the [predicate] is applied.
  */
-fun <ID : Any> Field<ID, Boolean>.all(default: Boolean, predicate: (Boolean) -> Boolean = { it }): Boolean =
-    fold(predicate(default)) { acc, value -> acc && predicate(value) }
+@JvmOverloads
+fun <ID : Any> Field<ID, Boolean>.all(base: Boolean, predicate: (Boolean) -> Boolean = { it }): Boolean =
+    fold(predicate(base)) { acc, value -> acc && predicate(value) }
 
 /**
  * Check if any of the [Boolean] elements in the field are true by default.
  * If [predicate] is defined, it is applied to the elements.
- * The local value is not considered, unless explicitly passed as [default].
+ * The [base] value is used as a first element to start the fold operation at which the [predicate] is applied.
  */
-fun <ID : Any> Field<ID, Boolean>.any(default: Boolean, predicate: (Boolean) -> Boolean = { it }): Boolean =
-    fold(predicate(default)) { acc, value -> acc || predicate(value) }
+@JvmOverloads
+fun <ID : Any> Field<ID, Boolean>.any(base: Boolean, predicate: (Boolean) -> Boolean = { it }): Boolean =
+    fold(predicate(base)) { acc, value -> acc || predicate(value) }
 
 /**
  * Check if none of the [Boolean] elements in the field are true by default.
  * If [predicate] is defined, it is applied to the elements.
- * The local value is not considered, unless explicitly passed as [default].
+ * The [base] value is used as a first element to start the fold operation at which the [predicate] is applied.
  */
-fun <ID : Any> Field<ID, Boolean>.none(default: Boolean, predicate: (Boolean) -> Boolean = { it }): Boolean =
-    !all(default, predicate)
+@JvmOverloads
+fun <ID : Any> Field<ID, Boolean>.none(base: Boolean, predicate: (Boolean) -> Boolean = { it }): Boolean =
+    !all(base, predicate)
