e582e3bdaf15c8f9a1316bb0fbcb6431ea9f176a
FreshMag <pescomagnani@gmail.com>
2024-08-19 15:50:28 +0200

    refactor: removed useless line inside testing resource

--- compiler-plugin-test/src/test/resources/TestAggregateInLoop.kt
+++ compiler-plugin-test/src/test/resources/TestAggregateInLoop.kt
@@ -5,8 +5,7 @@ fun Aggregate<Int>.exampleAggregate(): Unit =
     println()
 
 fun Aggregate<Int>.x() {
-    val x = 10
     for (j in listOf(1, 2, 3)) {
-        this.exampleAggregate() // Warning: aggregate function called inside a loop with no manual alignment operation
+        exampleAggregate() // Warning: aggregate function called inside a loop with no manual alignment operation
     }
 }
