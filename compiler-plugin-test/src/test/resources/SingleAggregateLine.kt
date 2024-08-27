44d7f673a547a8aeaae8ef0f1a05be98d3ff58e4
FreshMag <pescomagnani@gmail.com>
2024-08-27 11:17:36 +0200

    docs: added comments inside Kotlin testing sources

--- compiler-plugin-test/src/test/resources/SingleAggregateLine.kt
+++ compiler-plugin-test/src/test/resources/SingleAggregateLine.kt
@@ -1,5 +1,10 @@
 import it.unibo.collektive.aggregate.api.Aggregate
 
+/**
+    Kotlin program template with a single template line.
+    %s is a placeholder for string formatting that allows putting an arbitrary
+    piece of code inside it (avoiding having to create multiple testing sources).
+*/
 fun Aggregate<Int>.x() {
     %s
 }
--- compiler-plugin-test/src/test/resources/TestAggregateInLoop.kt
+++ compiler-plugin-test/src/test/resources/TestAggregateInLoop.kt
@@ -2,6 +2,12 @@ import it.unibo.collektive.aggregate.api.Aggregate
 
 fun Aggregate<Int>.exampleAggregate(): Int = 0
 
+/**
+    Kotlin program template with four template lines: before/after the loop and
+    before/after the aggregate call inside the loop.
+    %s is a placeholder for string formatting that allows putting an arbitrary
+    piece of code inside it (avoiding having to create multiple testing sources).
+*/
 fun Aggregate<Int>.x(pivot: (Int) -> Any?) {
     %s
     for (j in listOf(1, 2, 3)) {
