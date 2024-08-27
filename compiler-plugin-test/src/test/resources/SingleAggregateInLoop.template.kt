823b47c49b5f7f32d6bb77bf3346b474db94daf2
FreshMag <pescomagnani@gmail.com>
2024-08-27 18:32:35 +0200

    refactor: changed testing resource to match new string interpolation system

--- compiler-plugin-test/src/test/resources/SingleAggregateInLoop.template.kt
+++ compiler-plugin-test/src/test/resources/SingleAggregateInLoop.template.kt
@@ -1,19 +1,20 @@
 import it.unibo.collektive.aggregate.api.Aggregate
+%(imports)
 
 fun Aggregate<Int>.exampleAggregate(): Int = 0
 
 /**
     Kotlin program template with four template lines: before/after the loop and
     before/after the aggregate call inside the loop.
-    %%s is a placeholder for string formatting that allows putting an arbitrary
-    piece of code inside it (avoiding having to create multiple testing sources).
+    The parts after "%" are placeholders for string formatting that allow putting an arbitrary
+    piece of code inside them (avoiding having to create multiple testing sources).
 */
 fun Aggregate<Int>.x(pivot: (Int) -> Any?) {
-    %s
+    %(beforeLoop)
     for (j in listOf(1, 2, 3)) {
-        %s
-        exampleAggregate()
-        %s
+        %(beforeAggregate)
+        %(aggregate)
+        %(afterAggregate)
     }
-    %s
+    %(afterLoop)
 }
