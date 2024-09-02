acaadc4f31c63258a1e37b6483df1f5e844c8eb0
FreshMag <pescomagnani@gmail.com>
2024-09-02 16:15:55 +0200

    docs: trasformed CheckersUtility.kt docs into markdown syntax

--- compiler-plugin/src/main/kotlin/it/unibo/collektive/frontend/checkers/CheckersUtility.kt
+++ compiler-plugin/src/main/kotlin/it/unibo/collektive/frontend/checkers/CheckersUtility.kt
@@ -29,18 +29,23 @@ object CheckersUtility {
     }
 
     /**
-     * Checks is a specific receiver parameter is `Aggregate` (`Aggregate<ID>.example()`).
-     * @param session [FirSession] session of the [CheckerContext].
-     * @return [Boolean] **true** if it's an `Aggregate` function, **false** otherwise.
+     * Checks is a specific receiver parameter is [Aggregate][it.unibo.collektive.aggregate.api.Aggregate]
+     * (`Aggregate<ID>.example()`).
+     * It uses the [session] of the [CheckerContext] in which this check is performed to get the class symbol of the
+     * parameter.
+     *
+     * It returns **true** if it's an `Aggregate` function, **false** otherwise.
      */
     fun FirReceiverParameter.isAggregate(session: FirSession): Boolean =
         typeRef.toClassLikeSymbol(session)?.name?.asString() == "Aggregate"
 
     /**
-     * Checks if the function that is called is an `Aggregate` one (i.e. is an extension of `Aggregate` or it's a
-     * method of the `Aggregate` class)
-     * @param session [FirSession] session of the [CheckerContext].
-     * @return [Boolean] **true** if it's an `Aggregate` function, **false** otherwise.
+     * Checks if the function that is called is an [Aggregate][it.unibo.collektive.aggregate.api.Aggregate] one
+     * (i.e. is an extension of `Aggregate` or it's a method of the `Aggregate` class).
+     * It uses the [session] of the [CheckerContext] in which this check is performed to get the class symbol of the
+     * receiver parameter.
+     *
+     * It returns **true** if it's an `Aggregate` function, **false** otherwise.
      */
     fun FirFunctionCall.isAggregate(session: FirSession): Boolean {
         val callableSymbol = toResolvedCallableSymbol()
@@ -49,8 +54,10 @@ object CheckersUtility {
     }
 
     /**
-     * Checks if the current [CheckerContext] is wrapped inside an `Aggregate` function.
-     * @return [Boolean] **true** if it is wrapped inside an `Aggregate` function, **false** otherwise.
+     * Checks if the current [CheckerContext] is wrapped inside an
+     * [Aggregate][it.unibo.collektive.aggregate.api.Aggregate] function.
+     *
+     * It returns **true** if it is wrapped inside an `Aggregate` function, **false** otherwise.
      */
     fun CheckerContext.isInsideAggregateFunction(): Boolean =
         containingElements.any { (it as? FirSimpleFunction)?.receiverParameter?.isAggregate(session) == true }
