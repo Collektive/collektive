5377d76190a0ce5723b85a79ed3f34b2bfb5feca
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2024-10-18 17:16:18 +0200

    build: add command for website deployment

--- release.config.mjs
+++ release.config.mjs
@@ -1,6 +1,9 @@
 const publishCmd = `
-./gradlew gradle-plugin:publishPlugins -Pgradle.publish.key=$GRADLE_PUBLISH_KEY -Pgradle.publish.secret=$GRADLE_PUBLISH_SECRET || exit 1
-./gradlew -PstagingRepositoryId=\${process.env.STAGING_REPO_ID} releaseStagingRepositoryOnMavenCentral || exit 2
+git -C site/build add . || exit 1
+git -C site/build commit -m "chore: update website to version \${nextRelease.version}" || exit 2
+git -C site/build push || exit 3
+./gradlew gradle-plugin:publishPlugins -Pgradle.publish.key=$GRADLE_PUBLISH_KEY -Pgradle.publish.secret=$GRADLE_PUBLISH_SECRET || exit 4
+./gradlew -PstagingRepositoryId=\${process.env.STAGING_REPO_ID} releaseStagingRepositoryOnMavenCentral || exit 5
 `;
 
 import config from 'semantic-release-preconfigured-conventional-commits' assert { type: "json" }
