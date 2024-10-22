e42e0a82815190f8ca1aa80c6313274d483d3e92
FreshMag <pescomagnani@gmail.com>
2024-10-22 12:39:21 +0200

    test: added comment at the start of each generated file

--- compiler-plugin-test/src/test/resources/kotlin/ItListOfForEachNeighboring.kt
+++ compiler-plugin-test/src/test/resources/kotlin/ItListOfForEachNeighboring.kt
@@ -1,3 +1,7 @@
+/**
+ * This file has been auto-generated. See 
+ * [https://github.com/FreshMag/collektive-plugin/blob/test/generated-cases/compiler-plugin-test/src/test/resources/yaml/TestingCases.yaml](this link).  
+ */
 import it.unibo.collektive.aggregate.api.Aggregate
 
 fun Aggregate<Int>.entry() {
--- compiler-plugin-test/src/test/resources/kotlin/ItNstFunListOfForEachNeighboring.kt
+++ compiler-plugin-test/src/test/resources/kotlin/ItNstFunListOfForEachNeighboring.kt
@@ -1,5 +1,8 @@
+/**
+ * This file has been auto-generated. See 
+ * [https://github.com/FreshMag/collektive-plugin/blob/test/generated-cases/compiler-plugin-test/src/test/resources/yaml/TestingCases.yaml](this link).  
+ */
 import it.unibo.collektive.aggregate.api.Aggregate
-import it.unibo.collektive.Collektive.Companion.aggregate
 
 fun Aggregate<Int>.entry() {
    listOf(1,2,3).forEach {
