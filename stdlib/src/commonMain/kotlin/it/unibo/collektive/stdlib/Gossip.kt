84de96f7b676b4e9cf18c3489831fb5a9ceb97bf
Angela Cortecchia <angela.cortecchia@studio.unibo.it>
2024-10-22 16:19:50 +0200

    feat: add aggregate gossip algorithm

---
+++ stdlib/src/commonMain/kotlin/it/unibo/collektive/stdlib/Gossip.kt
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2024, Danilo Pianini, Nicolas Farabegoli, Elisa Tronetti,
+ * and all authors listed in the `build.gradle.kts` and the generated `pom.xml` file.
+ *
+ * This file is part of Collektive, and is distributed under the terms of the Apache License 2.0,
+ * as described in the LICENSE file in this project's repository's top directory.
+ */
+
+package it.unibo.collektive.stdlib
+
+import it.unibo.collektive.aggregate.api.Aggregate
+import it.unibo.collektive.aggregate.api.operators.share
+import it.unibo.collektive.field.Field.Companion.fold
+
+/**
+ * The [value] exchanged in the gossip algorithm.
+ * It contains the [value] itself and the [path] of nodes through which it has passed.
+ */
+private data class GossipValue<ID : Comparable<ID>, Value>(val value: Value, val path: List<ID>)
+
+/**
+ * Gossip algorithm implementation.
+ * Each node starts with an [initial] value, shares it with its neighbors,
+ * and updates its value based on the best value received according to the [selector].
+ */
+fun <ID : Comparable<ID>, Value> Aggregate<ID>.gossip(
+    initial: Value,
+    selector: (Value, Value) -> Boolean,
+): Value {
+    val local = GossipValue(initial, emptyList<ID>())
+    return share(local) { gossip ->
+        gossip.fold(local) { current, next ->
+            val selected = when {
+                selector(current.value, next.value) || localId in next.path -> current
+                else -> next
+            }
+            selected.copy(path = selected.path + localId)
+        }
+    }.value
+}
