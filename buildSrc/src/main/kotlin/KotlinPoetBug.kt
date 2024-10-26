a0f68d858d74add8d2d9ac2c50ca47ab22e31046
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2024-10-26 12:25:01 +0200

    style: remove warning

--- buildSrc/src/main/kotlin/KotlinPoetBug.kt
+++ buildSrc/src/main/kotlin/KotlinPoetBug.kt
@@ -1,7 +1,7 @@
 import com.squareup.kotlinpoet.asTypeName
 import kotlin.reflect.jvm.kotlinFunction
 
-fun main(args: Array<String>) {
+fun main() {
     Class.forName("kotlin.collections.ArraysKt").methods.first {
         it.name == "max" && it.parameters.first().parameterizedType.typeName.contains("Comparable")
     }.kotlinFunction?.parameters?.first()?.type?.asTypeName()
