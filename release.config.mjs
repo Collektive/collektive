e158bf40634fd16344fc1b057dd781de4f26697a
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2024-10-27 10:41:10 +0100

    build: properly manage site pubblication when no changes to the website are performed but a release is required

--- release.config.mjs
+++ release.config.mjs
@@ -1,12 +1,18 @@
 const publishCmd = `
-git -C site/build add . || exit 1
-git -C site/build commit -m "chore: update website to version \${nextRelease.version}" || exit 2
-git -C site/build push || exit 3
-./gradlew gradle-plugin:publishPlugins -Pgradle.publish.key=$GRADLE_PUBLISH_KEY -Pgradle.publish.secret=$GRADLE_PUBLISH_SECRET || exit 4
-./gradlew -PstagingRepositoryId=\${process.env.STAGING_REPO_ID} releaseStagingRepositoryOnMavenCentral || exit 5
+if [ -n "\$(git status --porcelain)" ]; then
+    echo "site directory changes detected, new site release..."
+    git -C site/build add . || exit 1
+    git -C site/build commit -m "chore: update website to version \${nextRelease.version}" || exit 2
+else
+    echo "no site directory changes detected, pushing empty commit..."
+    git -C site/build commit --allow-empty -m "chore: new release \${nextRelease.version}, no website updates" || exit 3
+fi
+git -C site/build push || exit 4
+./gradlew gradle-plugin:publishPlugins -Pgradle.publish.key=$GRADLE_PUBLISH_KEY -Pgradle.publish.secret=$GRADLE_PUBLISH_SECRET || exit 5
+./gradlew -PstagingRepositoryId=\${process.env.STAGING_REPO_ID} releaseStagingRepositoryOnMavenCentral || exit 6
 `;
 
-import config from 'semantic-release-preconfigured-conventional-commits' assert { type: "json" }
+import config from 'semantic-release-preconfigured-conventional-commits' assert {type: 'json'}
 
 config.plugins.push(
     [
