e3bfa0d43552b500a7a18de0dcdde10698c12e34
Angela Cortecchia <angela.cortecchia@studio.unibo.it>
2024-10-30 10:22:36 +0100

    test: add tests on hood, hoodWithId and fold

--- dsl/src/commonTest/kotlin/it/unibo/collektive/field/FieldOpsTest.kt
+++ dsl/src/commonTest/kotlin/it/unibo/collektive/field/FieldOpsTest.kt
@@ -5,17 +5,34 @@ import io.kotest.matchers.collections.shouldContainAll
 import io.kotest.matchers.equals.shouldNotBeEqual
 import io.kotest.matchers.shouldBe
 import it.unibo.collektive.field.Field.Companion.fold
+import it.unibo.collektive.field.Field.Companion.hood
+import it.unibo.collektive.field.Field.Companion.hoodWithId
 
 class FieldOpsTest : StringSpec({
     val emptyField = Field(0, "localVal")
     val fulfilledField = Field(0, 0, mapOf(1 to 10, 2 to 20))
 
+    "An empty field should return the default value value when is hooded" {
+        emptyField.hood("default") { acc, elem -> acc + elem } shouldBe "default"
+    }
+
+    "A non-empty field should not be hooded on default value" {
+        fulfilledField.hood(42) { acc, elem -> acc + elem } shouldBe 30
+    }
+
+    "A non-empty field hooded with Id should return a punctual value related to an evaluation over the id" {
+        val field = Field(0, 0, mapOf(1 to 10, 2 to 20, 3 to 15))
+        field.hoodWithId(42) { acc, id, elem -> if (id % 2 == 0) acc + elem else acc - elem } shouldBe 15
+    }
+
     "An empty field should return the self value value when is folded" {
         emptyField.fold(emptyField.localValue) { acc, elem -> acc + elem } shouldBe "localVal"
     }
-    "An empty field should return the initial value when is folded excluding self" {
-        emptyField.fold("initial") { acc, elem -> acc + elem } shouldBe "initial"
+
+    "A non-empty field should return the accumulated value when is folded excluding self" {
+        fulfilledField.fold(42) { acc, elem -> acc + elem } shouldBe 72
     }
+
     "An empty field when mapped only the local value should be transformed" {
         emptyField.map { "$it-mapped" } shouldBe Field(0, "localVal-mapped", mapOf())
     }
