b616ddbc2a1356a7277f2472559cfe40c3a778c5
Angela Cortecchia <angela.cortecchia@unibo.it>
2024-11-14 17:59:44 +0100

    refactor(dsl)!: rename repeat/repeating functions into evolve/evolving (#577)

--- dsl/src/commonTest/kotlin/it/unibo/collektive/aggregate/EvolvingTest.kt
+++ dsl/src/commonTest/kotlin/it/unibo/collektive/aggregate/EvolvingTest.kt
@@ -11,40 +11,40 @@ import it.unibo.collektive.network.NetworkImplTest
 import it.unibo.collektive.network.NetworkManager
 import it.unibo.collektive.stdlib.ints.FieldedInts.plus
 
-class RepeatingTest : StringSpec({
+class EvolvingTest : StringSpec({
     val id0 = 0
     val id1 = 1
 
     val double: (Int) -> Int = { it * 2 }
     val initV1 = 1
 
-    "First time repeating" {
+    "First time evolving" {
         aggregate(id0) {
-            repeat(initV1, double) shouldBe 2
+            evolve(initV1, double) shouldBe 2
         }
     }
 
-    "Repeating more than once" {
+    "Evolving more than once" {
         var res = 0
 
         val testNetwork = NetworkImplTest(NetworkManager(), id1)
         aggregate(id1, testNetwork) {
-            res = repeat(initV1, double)
-            res = repeat(res, double)
+            res = evolve(initV1, double)
+            res = evolve(res, double)
         }
         res shouldBe 4
     }
 
-    "Repeat with lambda body should work fine" {
+    "Evolve with lambda body should work fine" {
         val testNetwork = NetworkImplTest(NetworkManager(), id1)
         aggregate(id1, testNetwork) {
-            repeat(initV1) { it * 2 } shouldBe 2
+            evolve(initV1) { it * 2 } shouldBe 2
         }
     }
 
-    "Repeating should return the value passed in the yielding function" {
+    "Evolving should return the value passed in the yielding function" {
         val result = aggregate(id1) {
-            repeating(initV1) {
+            evolving(initV1) {
                 val nbr = neighboringViaExchange(it * 2).localValue
                 nbr.yielding { "A string" }
             } shouldBe "A string"
@@ -54,21 +54,21 @@ class RepeatingTest : StringSpec({
         messages.values shouldContainAll listOf(2)
     }
 
-    "Repeating should work fine even with null as value" {
+    "Evolving should work fine even with null as value" {
         val testNetwork1 = NetworkImplTest(NetworkManager(), id1)
         aggregate(id1, testNetwork1) {
-            repeating(initV1) {
+            evolving(initV1) {
                 val mult = it * 2
                 mult.yielding { "Hello".takeIf { mult < 1 } }
             } shouldBe null
         }
     }
 
-    "When the repeating returns a Field an exception must be raised" {
+    "When the evolving returns a Field an exception must be raised" {
         shouldThrow<IllegalStateException> {
             aggregate(id1) {
                 exchange(0) { field ->
-                    repeat(field) { it + 1 }
+                    evolve(field) { it + 1 }
                 }
             }
         }
