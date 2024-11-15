678adedb23aeb37f60a8f49a6936ff0e5976cc7c
FreshMag <pescomagnani@gmail.com>
2024-11-15 18:04:58 +0100

    chore: updated link inside testing resources

--- compiler-plugin-test/src/test/resources/kotlin/IterationNeighboringForEach.kt
+++ compiler-plugin-test/src/test/resources/kotlin/IterationNeighboringForEach.kt
@@ -1,6 +1,6 @@
 /**
  * This file has been auto-generated.
- * See [https://github.com/FreshMag/subjekt/blob/main/generator/src/main/resources/subjects/IterationWithAggregate.yaml](here)
+ * See [https://github.com/FreshMag/collektive-plugin/blob/test/generated-cases/compiler-plugin-test/src/test/resources/yaml/IterationWithAggregate.yaml](here)
  * for details.
 */
 import it.unibo.collektive.aggregate.api.Aggregate
