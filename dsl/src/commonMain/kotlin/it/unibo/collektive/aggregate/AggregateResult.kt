2ff84a87b5d3cf95bc13d58270a7a7a1eb8f7103
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2024-11-30 09:29:41 +0100

    refactor: remove empty method as no longer needed

--- dsl/src/commonMain/kotlin/it/unibo/collektive/aggregate/AggregateResult.kt
+++ dsl/src/commonMain/kotlin/it/unibo/collektive/aggregate/AggregateResult.kt
@@ -8,22 +8,9 @@ import it.unibo.collektive.state.State
  * It represents the [localId] of the device, the [result] of the computation,
  * the messages [toSend] to other devices and the [newState] of the device.
  */
-data class AggregateResult<ID : Any, R>(
+data class AggregateResult<ID : Any, out R>(
     val localId: ID,
     val result: R,
     val toSend: OutboundMessage<ID>,
     val newState: State,
-) {
-    /**
-     * Utilities for the [newState] of the device.
-     */
-    companion object {
-        /**
-         * Create an empty [AggregateResult] with the [localId] of the device, the [baseResult] of the computation.
-         */
-        fun <ID : Any, R> empty(
-            localId: ID,
-            baseResult: R,
-        ): AggregateResult<ID, R> = AggregateResult(localId, baseResult, OutboundMessage(0, localId), emptyMap())
-    }
-}
+)
