ce70414bafabd7c87a85d35ce0c7b13142ac7f87
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2024-12-06 12:20:56 +0100

    build: reduce boilerplate in detekt sarif configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,7 @@ plugins {
 }
 val reportMerge by tasks.registering(ReportMergeTask::class) {
     output = project.layout.buildDirectory.file("reports/detekt/deteket-merge.sarif")
+    input.from(tasks.withType<Detekt>().map { it.sarifReportFile })
 }
 
 allprojects {
@@ -129,9 +130,6 @@ allprojects {
     }
 
     tasks.withType<Detekt>().configureEach { finalizedBy(reportMerge) }
-    reportMerge {
-        input.from(tasks.withType<Detekt>().map { it.sarifReportFile })
-    }
 
     tasks.withType<Cpd>().configureEach {
         exclude {
