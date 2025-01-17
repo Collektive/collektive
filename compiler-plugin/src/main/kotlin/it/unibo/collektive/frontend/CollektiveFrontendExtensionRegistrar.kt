fbb0337cdaee87a0857954268e975dbf5f78cf0b
Francesco Magnani <70514090+FreshMag@users.noreply.github.com>
2025-01-17 16:09:40 +0100

    feat(frontend-compiler-plugin): identify wrong use of pattern `evolve` + `neighboring` instead of using `share` (#693)

--- compiler-plugin/src/main/kotlin/it/unibo/collektive/frontend/CollektiveFrontendExtensionRegistrar.kt
+++ compiler-plugin/src/main/kotlin/it/unibo/collektive/frontend/CollektiveFrontendExtensionRegistrar.kt
@@ -6,8 +6,8 @@ import org.jetbrains.kotlin.fir.extensions.FirExtensionRegistrar
  * Extension registrar that adds frontend extensions to the compiler. It checks for dangerous or inappropriate code
  * written using the Collektive DSL
  */
-class AlignmentFrontendExtensionRegistrar : FirExtensionRegistrar() {
+class CollektiveFrontendExtensionRegistrar : FirExtensionRegistrar() {
     override fun ExtensionRegistrarContext.configurePlugin() {
-        +::MissingAlignExtension
+        +::CollektiveExtension
     }
 }
