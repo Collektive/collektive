ff5615d47494d88372523c3f470e60daad35deb9
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2025-02-08 00:14:19 +0100

    refactor: use inlined neighboring version

--- alchemist-incarnation-collektive/src/main/kotlin/it/unibo/alchemist/collektive/device/CollektiveDevice.kt
+++ alchemist-incarnation-collektive/src/main/kotlin/it/unibo/alchemist/collektive/device/CollektiveDevice.kt
@@ -7,6 +7,7 @@ import it.unibo.alchemist.model.Position
 import it.unibo.alchemist.model.Time
 import it.unibo.alchemist.model.molecules.SimpleMolecule
 import it.unibo.collektive.aggregate.api.Aggregate
+import it.unibo.collektive.aggregate.api.Aggregate.Companion.neighboring
 import it.unibo.collektive.alchemist.device.sensors.EnvironmentVariables
 import it.unibo.collektive.field.Field
 import it.unibo.collektive.networking.Mailbox
