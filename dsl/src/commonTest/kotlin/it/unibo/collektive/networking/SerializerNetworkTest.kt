3b9360aba0bf6b80e37e7ca78cac764bfeaa86d7
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2025-02-08 01:08:42 +0100

    test: make serializer network independent of the serialization strategy

--- dsl/src/commonTest/kotlin/it/unibo/collektive/networking/SerializerNetworkTest.kt
+++ dsl/src/commonTest/kotlin/it/unibo/collektive/networking/SerializerNetworkTest.kt
@@ -9,21 +9,39 @@
 package it.unibo.collektive.networking
 
 import it.unibo.collektive.path.Path
+import kotlinx.serialization.BinaryFormat
+import kotlinx.serialization.SerialFormat
+import kotlinx.serialization.StringFormat
+import kotlinx.serialization.decodeFromByteArray
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.encodeToByteArray
+import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import kotlin.reflect.KClass
 
-class SerializerNetworkTest : Mailbox<Int> {
-    private val serializer = Json
+class SerializerNetworkTest(private val serializer: SerialFormat = Json) : Mailbox<Int> {
     val messages = mutableMapOf<Int, Message<Int, Any?>>()
     private val factory = object : SerializedMessageFactory<Int, Any?>(serializer) {}
 
     fun serializeAndSend(id: Int): String {
         require(messages[id] is SerializedMessage<Int>) { "Message $id is not serialized" }
-        return serializer.encodeToString(messages[id] as SerializedMessage<Int>)
+        return when (serializer) {
+            is StringFormat -> serializer.encodeToString(messages[id] as SerializedMessage<Int>)
+            is BinaryFormat -> serializer.encodeToByteArray(messages[id] as SerializedMessage<Int>).decodeToString()
+            else -> error("Unsupported serializer")
+        }
     }
 
     fun deserializeAndReceive(serialized: String) {
-        val message = serializer.decodeFromString<SerializedMessage<Int>>(serialized)
+        val message =
+            when (serializer) {
+                is StringFormat -> serializer.decodeFromString<SerializedMessage<Int>>(serialized)
+                is BinaryFormat ->
+                    serializer.decodeFromByteArray<SerializedMessage<Int>>(
+                        serialized.encodeToByteArray(),
+                    )
+                else -> error("Unsupported serializer")
+            }
         deliverableReceived(message)
     }
 
