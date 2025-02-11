1029a71b0f1a503e365b7476f6c6e40b4c875f76
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2025-02-11 21:20:42 +0100

    test: remove kotest and minor refactors

--- dsl/src/commonTest/kotlin/it/unibo/collektive/branch/IfConditionTest.kt
+++ dsl/src/commonTest/kotlin/it/unibo/collektive/branch/IfConditionTest.kt
@@ -1,78 +1,82 @@
 package it.unibo.collektive.branch
 
-import io.kotest.core.spec.style.StringSpec
-import io.kotest.matchers.maps.shouldHaveSize
-import io.kotest.matchers.shouldBe
 import it.unibo.collektive.Collektive.Companion.aggregate
 import it.unibo.collektive.aggregate.api.operators.neighboringViaExchange
+import kotlin.test.Test
+import kotlin.test.assertEquals
 
-class IfConditionTest : StringSpec({
-    val id0 = 0
-    "Branches with constant conditions should get aligned" {
+class IfConditionTest {
+    @Test
+    fun `Branches with constant conditions should get aligned`() {
         val result =
-            aggregate(id0) {
+            aggregate(0) {
                 if (true) neighboringViaExchange("test")
             }
-        val messageFor0 = result.toSend.prepareMessageFor(id0).sharedData
-        messageFor0 shouldHaveSize 1
-        messageFor0.values.toList() shouldBe listOf("test")
+        val messageFor0 = result.toSend.prepareMessageFor(0).sharedData
+        assertEquals(1, messageFor0.size)
+        assertEquals(listOf("test"), messageFor0.values.toList())
     }
 
-    "Branches with conditions read from variables should get aligned" {
+    @Test
+    fun `Branches with conditions read from variables should get aligned`() {
         val customCondition = true
         val result =
-            aggregate(id0) {
+            aggregate(0) {
                 if (customCondition) neighboringViaExchange("test")
             }
-        val messageFor0 = result.toSend.prepareMessageFor(id0).sharedData
-        messageFor0 shouldHaveSize 1
-        messageFor0.values.toList() shouldBe listOf("test")
+        val messageFor0 = result.toSend.prepareMessageFor(0).sharedData
+        assertEquals(1, messageFor0.size)
+        assertEquals(listOf("test"), messageFor0.values.toList())
     }
 
-    "Function condition if" {
+    @Test
+    fun `Function condition if`() {
         fun customFunction() = true
         val result =
-            aggregate(id0) {
+            aggregate(0) {
                 if (customFunction()) neighboringViaExchange("test")
             }
-        val messageFor0 = result.toSend.prepareMessageFor(id0).sharedData
-        messageFor0 shouldHaveSize 1
-        messageFor0.values.toList() shouldBe listOf("test")
+        val messageFor0 = result.toSend.prepareMessageFor(0).sharedData
+        assertEquals(1, messageFor0.size)
+        assertEquals(listOf("test"), messageFor0.values.toList())
     }
 
-    "Function and condition if" {
+    @Test
+    fun `Function and condition if`() {
         val customCondition1 = true
         val customCondition2 = true
         val result =
-            aggregate(id0) {
+            aggregate(0) {
                 if (customCondition1 && customCondition2) neighboringViaExchange("test")
             }
-        val messageFor0 = result.toSend.prepareMessageFor(id0).sharedData
-        messageFor0 shouldHaveSize 1
-        messageFor0.values.toList() shouldBe listOf("test")
+        val messageFor0 = result.toSend.prepareMessageFor(0).sharedData
+        assertEquals(1, messageFor0.size)
+        assertEquals(listOf("test"), messageFor0.values.toList())
     }
 
-    "Function or condition if" {
+    @Test
+    fun `Function or condition if`() {
         val customCondition1 = true
         val customCondition2 = true
         val result =
-            aggregate(id0) {
+            aggregate(0) {
                 if (customCondition1 || customCondition2) neighboringViaExchange("test")
             }
-        val messageFor0 = result.toSend.prepareMessageFor(id0).sharedData
-        messageFor0 shouldHaveSize 1
-        messageFor0.values.toList() shouldBe listOf("test")
+        val messageFor0 = result.toSend.prepareMessageFor(0).sharedData
+        assertEquals(1, messageFor0.size)
+        assertEquals(listOf("test"), messageFor0.values.toList())
     }
 
-    "Function not condition if" {
+    @Test
+    fun `Function not condition if`() {
         val customCondition1 = true
         val customCondition2 = false
         val result =
-            aggregate(id0) {
+            aggregate(0) {
                 if (customCondition1 && !customCondition2) neighboringViaExchange("test")
             }
-        val messageFor0 = result.toSend.prepareMessageFor(id0).sharedData
-        messageFor0 shouldHaveSize 1
-        messageFor0.values.toList() shouldBe listOf("test")
+        val messageFor0 = result.toSend.prepareMessageFor(0).sharedData
+        assertEquals(1, messageFor0.size)
+        assertEquals(listOf("test"), messageFor0.values.toList())
     }
-})
+}
