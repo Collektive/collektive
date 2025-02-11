23f4405a3c7c1fc41696be3a1830f1f98da04444
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2025-02-11 21:50:34 +0100

    refactor: avoid code duplications

--- dsl/src/commonTest/kotlin/it/unibo/collektive/matchers/AggregateResultMatchers.kt
+++ dsl/src/commonTest/kotlin/it/unibo/collektive/matchers/AggregateResultMatchers.kt
@@ -3,8 +3,13 @@ package it.unibo.collektive.matchers
 import it.unibo.collektive.Collektive.Companion.aggregate
 import it.unibo.collektive.aggregate.api.Aggregate
 import it.unibo.collektive.networking.NoNeighborsData
+import it.unibo.collektive.path.Path
 import kotlin.test.assertEquals
 import kotlin.test.assertNotEquals
+
+private fun <Result> getSharedDataKeys(program: Aggregate<Int>.() -> Result): Set<Path> =
+    aggregate(0, emptyMap(), NoNeighborsData(), compute = program)
+        .run { toSend.prepareMessageFor(this.localId).sharedData.keys }
 
 /**
  * Asserts that the [firstProgram] and [secondProgram] align.
@@ -13,12 +18,8 @@ fun <Result> assertAligned(
     firstProgram: Aggregate<Int>.() -> Result,
     secondProgram: Aggregate<Int>.() -> Result,
 ) {
-    val first =
-        aggregate(0, emptyMap(), NoNeighborsData(), compute = firstProgram)
-            .run { toSend.prepareMessageFor(this.localId).sharedData.keys }
-    val second =
-        aggregate(0, emptyMap(), NoNeighborsData(), compute = secondProgram)
-            .run { toSend.prepareMessageFor(this.localId).sharedData.keys }
+    val first = getSharedDataKeys(firstProgram)
+    val second = getSharedDataKeys(secondProgram)
     assertEquals(first, second)
 }
 
@@ -29,11 +30,7 @@ fun <First, Second> assertNotAligned(
     firstProgram: Aggregate<Int>.() -> First,
     secondProgram: Aggregate<Int>.() -> Second,
 ) {
-    val first =
-        aggregate(0, emptyMap(), NoNeighborsData(), compute = firstProgram)
-            .run { toSend.prepareMessageFor(this.localId).sharedData.keys }
-    val second =
-        aggregate(0, emptyMap(), NoNeighborsData(), compute = secondProgram)
-            .run { toSend.prepareMessageFor(this.localId).sharedData.keys }
+    val first = getSharedDataKeys(firstProgram)
+    val second = getSharedDataKeys(secondProgram)
     assertNotEquals(first, second)
 }
