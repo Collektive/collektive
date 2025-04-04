7f8709335bb00231ddd9eeb19693cd0b992dd126
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2025-04-04 09:28:46 +0200

    build: remove multijvm from buildSrc

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     `kotlin-dsl`
-    id("org.danilopianini.multi-jvm-test-plugin") version "3.3.0"
 }
 
 repositories {
@@ -12,6 +11,5 @@ with(extensions.getByType<VersionCatalogsExtension>().named("libs")) {
     dependencies {
         implementation(kotlin("reflect"))
         implementation(findLibrary("kotlin-gradle-plugin").get())
-        implementation(findLibrary("kotlinpoet").get())
     }
 }
