7066c5e5188a03246f5ffb71698856478bb9fadf
Danilo Pianini <danilo.pianini@unibo.it>
2025-04-04 17:08:12 +0200

    build: allow ten seconds for js tests

--- buildSrc/src/main/kotlin/KmpConfig.kt
+++ buildSrc/src/main/kotlin/KmpConfig.kt
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2025, Danilo Pianini, Nicolas Farabegoli, Elisa Tronetti,
+ * and all authors listed in the `build.gradle.kts` and the generated `pom.xml` file.
+ *
+ * This file is part of Collektive, and is distributed under the terms of the Apache License 2.0,
+ * as described in the LICENSE file in this project's repository's top directory.
+ */
+
 import org.gradle.api.Project
 import org.gradle.api.provider.Provider
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
@@ -49,8 +57,20 @@ fun Project.configureKotlinMultiplatform() {
             }
         }
         js(IR) {
-            browser()
-            nodejs()
+            browser {
+                testTask {
+                    useMocha {
+                        timeout = "10000"
+                    }
+                }
+            }
+            nodejs {
+                testTask {
+                    useMocha {
+                        timeout = "10000"
+                    }
+                }
+            }
             binaries.library()
         }
         val nativeSetup: KotlinNativeTarget.() -> Unit = {
