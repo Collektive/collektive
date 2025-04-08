26f62c45c3fde2303c7173026987fc5915f51f3f
Danilo Pianini <danilo.pianini@unibo.it>
2025-04-08 17:37:30 +0200

    build(alchemist-incarnation-collektive): make the DSL and the stdlib API-level dependencies to simplify the import

--- alchemist-incarnation-collektive/build.gradle.kts
+++ alchemist-incarnation-collektive/build.gradle.kts
@@ -1,9 +1,17 @@
+/*
+ * Copyright (c) 2025, Danilo Pianini, Nicolas Farabegoli, Elisa Tronetti,
+ * and all authors listed in the `build.gradle.kts` and the generated `pom.xml` file.
+ *
+ * This file is part of Collektive, and is distributed under the terms of the Apache License 2.0,
+ * as described in the LICENSE file in this project's repository's top directory.
+ */
+
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.gradle.api.tasks.testing.logging.TestLogEvent
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
-    java
+    `java-library`
 }
 
 apply(plugin = rootProject.libs.plugins.kotlin.jvm.id)
@@ -20,10 +28,10 @@ kotlinJvm {
     sourceSets {
         val main by getting {
             dependencies {
+                api(project(":dsl"))
+                api(project(":stdlib"))
                 implementation("it.unibo.collektive:compiler-plugin")
                 implementation(project(":compiler-embeddable"))
-                implementation(project(":dsl"))
-                implementation(project(":stdlib"))
                 implementation(kotlin("reflect"))
                 implementation(libs.bundles.alchemist)
                 implementation(libs.bundles.kotlin.compiler)
