638326ffbe7514677a583e6983f2fb5c38085cc9
Angela Cortecchia <angela.cortecchia@unibo.it>
2025-04-10 18:34:34 +0200

    refactor: use kotlin test instead of kotest

--- compiler-plugin-test/src/test/kotlin/it/unibo/collektive/test/ImproperNeighboring.kt
+++ compiler-plugin-test/src/test/kotlin/it/unibo/collektive/test/ImproperNeighboring.kt
@@ -10,25 +10,18 @@ package it.unibo.collektive.test
 
 import io.github.subjekt.Subjekt.subjekt
 import io.github.subjekt.generators.FilesGenerator.toTempFiles
-import io.kotest.core.spec.style.FreeSpec
 import it.unibo.collektive.test.util.CompileUtils.getTestingProgram
 import it.unibo.collektive.test.util.CompileUtils.noWarning
+import kotlin.test.Test
 
-class ImproperNeighboring : FreeSpec({
-
-    "When using the 'neighboring' construct" - {
-        val testSubjects =
-            subjekt {
-                addSource("src/test/resources/subjekt/ImproperNeighboring.yaml")
-            }.toTempFiles()
-
-        "a valid use for the 'neighboring' construct, but not really useful" - {
-            val subjectName = "ProperUseNeighboringMapping"
-            val code = testSubjects.getTestingProgram(subjectName)
-
-            "should compile without warnings" - {
-                code shouldCompileWith noWarning
-            }
-        }
+class ImproperNeighboring {
+    @Test
+    fun `mapping the result of a neighboring should not throw a loop warning`() {
+        val testSubjects = subjekt {
+            addSource("src/test/resources/subjekt/ImproperNeighboring.yaml")
+        }.toTempFiles()
+        val subjectName = "ProperUseNeighboringMapping"
+        val code = testSubjects.getTestingProgram(subjectName)
+        code shouldCompileWith noWarning
     }
-})
+}
