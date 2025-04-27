b8173d58c1360229dde1d032cc6f95b7b98926f4
Danilo Pianini <danilo.pianini@gmail.com>
2025-04-27 17:53:42 +0200

    fix(alchemist-incarnation): fix broken gradient (#942)
    * fix(alchemist-incarnation): fix broken gradient
    
    * Update MyLib.kt

--- alchemist-incarnation-collektive/src/test/resources/it/unibo/collektive/test/kotlin/MyLib.kt
+++ alchemist-incarnation-collektive/src/test/resources/it/unibo/collektive/test/kotlin/MyLib.kt
@@ -3,6 +3,7 @@ package my.test
 import it.unibo.alchemist.collektive.device.CollektiveDevice
 import it.unibo.collektive.aggregate.api.Aggregate
 import it.unibo.collektive.aggregate.api.share
+import it.unibo.collektive.aggregate.api.mapNeighborhood
 import it.unibo.collektive.stdlib.doubles.FieldedDoubles.plus
 import it.unibo.collektive.stdlib.fields.minValue
 import kotlin.Double.Companion.POSITIVE_INFINITY
@@ -10,8 +11,9 @@ import kotlin.Double.Companion.POSITIVE_INFINITY
 fun Aggregate<Int>.gradient(distanceSensor: CollektiveDevice<*>, source: Boolean): Double =
     share(POSITIVE_INFINITY) {
         val dist = distanceSensor.run { distances() }
+        val throughNeighbor = (it + dist).minValue(POSITIVE_INFINITY)
         when {
             source -> 0.0
-            else -> (it + dist).minValue(POSITIVE_INFINITY)
+            else -> throughNeighbor
         }
     }
