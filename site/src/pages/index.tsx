5c41e6f0157e4188764ea9a550e3618efd334592
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2025-05-01 17:45:21 +0200

    docs: set collektive logo with rounded background for navbar and favicon

--- site/src/pages/index.tsx
+++ site/src/pages/index.tsx
@@ -7,7 +7,7 @@ import Heading from '@theme/Heading';
 
 import styles from './index.module.css';
 
-const SvgLogo = require('@site/static/img/collektive-logo-white.svg').default
+const SvgLogo = require('@site/static/img/collektive-logo-white-no-background.svg').default
 
 function HomepageHeader() {
     const {siteConfig} = useDocusaurusContext();
