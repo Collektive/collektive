1b4b38141f8df909642afe03b140dfb30fba3308
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2025-05-02 09:38:05 +0200

    docs(site): minor restyling on the home page

--- site/src/components/FeatureStrip/FeatureStrip.tsx
+++ site/src/components/FeatureStrip/FeatureStrip.tsx
@@ -10,26 +10,45 @@ import React from 'react';
 import clsx from 'clsx';
 import styles from './FeatureStrip.module.css';
 import "../../css/custom.css";
+import Heading from "@theme/Heading";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faArrowsLeftRight, faGears, faRocket} from "@fortawesome/free-solid-svg-icons";
 
 const features = [
     {
         title: 'Powerful DSL',
-        icon: <FontAwesomeIcon icon={faRocket} size="8x" color="var(--ifm-font-color-base)" />,
+        icon: <FontAwesomeIcon icon={faRocket} size="5x" color="var(--ifm-font-color-base)" />,
         description: 'Design collective systems using concise, type-safe Kotlin syntax.',
     },
     {
         title: 'Fully Multiplatform',
-        icon: <FontAwesomeIcon icon={faArrowsLeftRight} size="8x" color="var(--ifm-font-color-base)" />,
-        description: 'Run on JVM, JS, Native, Android, and iOS. Write once, run anywhere. For real.',
+        icon: <FontAwesomeIcon icon={faArrowsLeftRight} size="5x" color="var(--ifm-font-color-base)" />,
+        description: 'Run on JVM, JS, Native, Android, and iOS. Write once, run anywhere.',
     },
     {
         title: 'Composable Architecture',
-        icon: <FontAwesomeIcon icon={faGears} size="8x" color="var(--ifm-font-color-base)" />,
+        icon: <FontAwesomeIcon icon={faGears} size="5x" color="var(--ifm-font-color-base)" />,
         description: 'Compose your system from reusable, collective functions.',
     },
 ];
+
+function FeatureCard(input) {
+    return (
+        <div className={clsx("card-demo")}>
+            <div className={clsx("card")}>
+                <div className={clsx("card__header")}>
+                    <div className={styles.featureIcon}>
+                        {input.icon}
+                    </div>
+                </div>
+                <div className={clsx("card__body")}>
+                    <Heading as={"h2"}>{input.title}</Heading>
+                    <p>{input.description}</p>
+                </div>
+            </div>
+        </div>
+    )
+}
 
 export default function FeatureStrip() {
     return (
@@ -37,12 +56,7 @@ export default function FeatureStrip() {
             <div className="row">
                 {features.map((f, idx) => (
                     <div key={idx} className="col col--4 text--center">
-                        <div className={styles.featureIcon}>
-                            {f.icon}
-                        </div>
-                        <p/>
-                        <h3>{f.title}</h3>
-                        <p>{f.description}</p>
+                        <FeatureCard title={f.title} icon={f.icon} description={f.description} />
                     </div>
                 ))}
             </div>
