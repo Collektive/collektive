beae65ba10a331c68a76e77a1adce3ee8c1cd78c
Angela Cortecchia <angela.cortecchia@unibo.it>
2025-05-05 10:06:12 +0200

    fix: grammar

--- site/static/code/FirstCollektiveProgram.kt
+++ site/static/code/FirstCollektiveProgram.kt
@@ -1,15 +1,14 @@
 /**
  * Count the number of devices in the network.
- * The total is accumulated in the device with [localId] 0
- * (where the [sink] is true).
- * Other devices will have a value that represents the number of devices
- * from self (included) to the closest edge of the network.
+ * The total is accumulated in the device with [localId] 0 (the [sink]).
+ * Other devices hold the number of devices in their subtree towards the network edge, inlcuding themselves.
+ * A leaf node (with no outward neighbors) will hold 1.
  */
 fun Aggregate<Int>.deviceCounter(): Int = countDevices(sink = localId == 0)
 
 /**
- * Broadcast from the [source] the number of devices connected in the network.
- * Need a [distances] field used as metric to compute the distance from the source to the target.
+ * Broadcast from the [source] the number of devices connected to the network.
+ * Requires a [distances] field to be used as metric for computing the distance from the source to the target.
  */
 fun Aggregate<Int>.broadcastDevices(distances: Field<Int, Double>, source: Boolean): Int =
     gradientCast(
@@ -19,10 +18,10 @@ fun Aggregate<Int>.broadcastDevices(distances: Field<Int, Double>, source: Boole
     )
 
 /**
- * [collektiveDevice] is a representation of the device that runs a Collektive program.
+ * [collektiveDevice] represents the device running the Collektive program.
  * It is used to access the device's properties and methods,
  * such as the [distances] method, which returns a field of distances from the neighboring nodes.
- * In this case, the source is the device with [localId] 0.
+ * In this case, the source is the device with [localId] equal to 0.
  */
 fun Aggregate<Int>.broadcastDevicesEntrypoint(collektiveDevice: CollektiveDevice<*>): Int =
     broadcastDevices(
@@ -31,7 +30,8 @@ fun Aggregate<Int>.broadcastDevicesEntrypoint(collektiveDevice: CollektiveDevice
     )
 
 /**
- * Broadcast the number of devices connected in the network with a leader election.
+ * Counts and broadcast the number of devices in the network using a leader election.
+ * If the network is segmented, each connected component elects a leader to act as root.
  */
 fun Aggregate<Int>.broadcastDevicesWithLeaderElectionEntrypoint(
     collektiveDevice: CollektiveDevice<*>,
