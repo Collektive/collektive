8491d77601e75559891d5d71db96e7aa7e45cb4b
Angela Cortecchia <angela.cortecchia@unibo.it>
2025-05-26 14:39:18 +0200

    refactor: importing remark-math at the top

--- site/docusaurus.config.ts
+++ site/docusaurus.config.ts
@@ -2,6 +2,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 import {codeImport} from 'remark-code-import'
+import 'remark-math'
+import remarkMath from "remark-math";
 
 const config: Config = {
     title: 'Collektive',
@@ -41,7 +43,7 @@ const config: Config = {
                     // Remove this to remove the "edit this page" links.
                     editUrl:
                         'https://github.com/Collektive/collektive/tree/master/site/',
-                    remarkPlugins: [codeImport, require('remark-math')],
+                    remarkPlugins: [codeImport, remarkMath],
                     rehypePlugins: [require('rehype-katex')],
                 },
                 theme: {
