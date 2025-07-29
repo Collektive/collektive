c52658c9fdcc278423ecff60233e57e4afab82ab
Danilo Pianini <danilo.pianini@unibo.it>
2025-07-29 15:34:35 +0200

    fix(release): migrate to Maven Central Portal

--- release.config.mjs
+++ release.config.mjs
@@ -1,3 +1,14 @@
+/*
+ * Copyright (c) 2025, Danilo Pianini, Nicolas Farabegoli, Elisa Tronetti,
+ * and all authors listed in the `build.gradle.kts` and the generated `pom.xml` file.
+ *
+ * This file is part of Collektive, and is distributed under the terms of the Apache License 2.0,
+ * as described in the LICENSE file in this project's repository's top directory.
+ */
+
+const projects = ["", ":collektive-compiler-plugin:", ":collektive-gradle-plugin:", ":collektivize:"];
+const tasks = ["publishAllPublicationsToProjectLocalRepository", "zipMavenCentralPortalPublication", "releaseMavenCentralPortalPublication"];
+const gradleTasks = projects.flatMap(p => tasks.map(t => `${p}${t}`)).join(' ');
 const publishCmd = `
 if [ -n "\$(git -C site/build status --porcelain)" ]; then
     echo "site directory changes detected, new site release..."
@@ -10,10 +21,10 @@ fi
 git -C site/build push || exit 4
 ./gradlew -PforceVersion=\${nextRelease.version} collektive-gradle-plugin:publishPlugins -Pgradle.publish.key=$GRADLE_PUBLISH_KEY -Pgradle.publish.secret=$GRADLE_PUBLISH_SECRET || exit 5
 ./gradlew -PforceVersion=\${nextRelease.version} collektivize:publishPlugins -Pgradle.publish.key=$GRADLE_PUBLISH_KEY -Pgradle.publish.secret=$GRADLE_PUBLISH_SECRET || exit 6
-./gradlew -PstagingRepositoryId=\${process.env.STAGING_REPO_ID} releaseStagingRepositoryOnMavenCentral || exit 7
+./gradlew ${gradleTasks} || exit 7
 `;
 
-import config from 'semantic-release-preconfigured-conventional-commits' with {type: 'json'}
+import config from 'semantic-release-preconfigured-conventional-commits' with {type: 'json'};
 
 config.plugins.push(
     [
@@ -24,6 +35,6 @@ config.plugins.push(
     ],
     "@semantic-release/github",
     "@semantic-release/git",
-)
+);
 
 export default config;
