e239d34d1ea7ded360c9de686e8ad258f0d28a2b
Angela Cortecchia <angela.cortecchia@studio.unibo.it>
2025-11-14 16:43:47 +0100

    fix: adapt test with context parameters

--- collektive-stdlib/src/commonTest/kotlin/it/unibo/collektive/stdlib/test/SharedClockTest.kt
+++ collektive-stdlib/src/commonTest/kotlin/it/unibo/collektive/stdlib/test/SharedClockTest.kt
@@ -7,9 +7,9 @@
  */
 
 package it.unibo.collektive.stdlib.test
-
 import io.kotest.matchers.shouldBe
 import it.unibo.collektive.stdlib.test.MultiClock.Companion.DEVICE_COUNT
+import it.unibo.collektive.stdlib.test.MultiClock.Companion.ONE_SECOND
 import it.unibo.collektive.stdlib.time.sharedClock
 import it.unibo.collektive.testing.Environment
 import it.unibo.collektive.testing.mooreGrid
@@ -37,53 +37,69 @@ class SharedClockTest {
     @Test
     fun `In two subsequent rounds of computation the devices should increase their time by their delta seconds`() {
         // execution sequence: d0 -> d1 -> d2 -> d3 -> d0 -> d1 -> d2 -> d3
-        val env: Environment<Instant> = gridWithTimeIntervalBetweenRounds(SEQUENTIAL_FREQUENCY)
-        env.executeSubsequentRounds(times = 2)
-        val twoRounds = env.status().values.distinct()
-        twoRounds.size shouldBe 1
-        twoRounds.first() shouldBe DISTANT_PAST + SEQUENTIAL_FREQUENCY.seconds
+        with(MultiClock(DEVICE_COUNT)) {
+            val env: Environment<Instant> = gridWithTimeIntervalBetweenRounds()
+            env.runAllDevices(times = 2)
+            val twoRounds = env.status().values.distinct()
+            twoRounds.size shouldBe 1
+            twoRounds.first() shouldBe DISTANT_PAST + DEVICE_COUNT.seconds
+        }
     }
 
     @Test
     fun `In staggered rounds of computation the devices should increase their time based on the fastest device`() {
         // execution sequence: d0 -> (d1 -> d0) -> (d2 -> d1) -> (d3 -> d2) -> d3 -> d0 -> d1 -> d2 -> d3
-        val env: Environment<Instant> = gridWithTimeIntervalBetweenRounds(ONE_SEC_FREQUENCY)
-        for (n in 0 until NUM_DEVICES) {
-            env.nodes.elementAt(n).cycle()
-            if (n > 0) env.nodes.elementAt(n - 1).cycle()
+        with(MultiClock(DEVICE_COUNT)) {
+            val env: Environment<Instant> = gridWithTimeIntervalBetweenRounds(timeBetweenRounds = ONE_SECOND)
+            for (n in 0 until DEVICE_COUNT) {
+                env.nodes.elementAt(n).cycle()
+                if (n > 0) env.nodes.elementAt(n - 1).cycle()
+            }
+            env.nodes.last().cycle()
+            // change the device's execution frequency to execute after the time of the last device that has cycled
+            env.nodes.forEach { node ->
+                // minus one second because it has already been added after the first cycle
+                increaseTime(node.id, ONE_SECOND * (DEVICE_COUNT - ONE_SECOND))
+            }
+            env.cycleInOrder()
+            val offset = env.nodes.size + ONE_SECOND // each device gets an additional offset per round
+            val expected = listOf(offset, offset, offset + ONE_SECOND, offset + ONE_SECOND)
+            env.nodes.forEachIndexed { index, node ->
+                env.shouldBeInstant(
+                    node.id,
+                    DISTANT_PAST + expected[index].seconds,
+                )
+            }
         }
-        env.nodes.last().cycle()
-        // change the device's execution frequency to execute after the time of the last device that has cycled
-        env.nodes.forEach { node ->
-            // minus one second because it has already been added after the first cycle
-            increaseTime(node.id, ONE_SEC_FREQUENCY * (SEQUENTIAL_FREQUENCY - ONE_SEC_FREQUENCY))
-        }
-        env.cycleInOrder()
-        val offset = env.nodes.size + ONE_SEC_FREQUENCY // each device gets an additional offset per round
-        val expected = listOf(offset, offset, offset + ONE_SEC_FREQUENCY, offset + ONE_SEC_FREQUENCY)
-        env.nodes.forEachIndexed { index, node -> env.shouldBeInstant(node.id, DISTANT_PAST + expected[index].seconds) }
     }
 
     @Test
     fun `A single device that execute with a higher timestamp should put forward the time`() {
         // execution sequence: d0 -> d1 -> d2 -> d3 -> d0 -> d1 -> d2 -> d3 ->
         // -> d0 (drift time) -> d1 -> d2 -> d3 -> d1 -> d2 -> d3 (d0 is disappeared)
-        val env: Environment<Instant> = gridWithTimeIntervalBetweenRounds(SEQUENTIAL_FREQUENCY)
-        val subsequentRounds = 2
-        env.executeSubsequentRounds(times = subsequentRounds)
-        val node = env.nodes.find { it.id == 0 }
-        // put device 0 at virtual time 20
-        // (it was already at virtual time 8, as the timestamp has been increased twice by SEQUENTIAL_FREQUENCY)
-        val driftTime = SEQUENTIAL_FREQUENCY * 3
-        increaseTime(node!!.id, driftTime)
-        // execute a single round with the fast-forwarded device
-        env.executeSubsequentRounds(times = 1)
-        // simulate the death of device 0
-        env.nodes.filter { it.id != node.id }.forEach { n -> n.cycle() }
-        val deadDeviceTime = (SEQUENTIAL_FREQUENCY * subsequentRounds) + driftTime
-        val inSynch = deadDeviceTime + SEQUENTIAL_FREQUENCY
-        val expected = listOf(deadDeviceTime, inSynch, inSynch, inSynch)
-        env.nodes.forEachIndexed { index, node -> env.shouldBeInstant(node.id, DISTANT_PAST + expected[index].seconds) }
+        with(MultiClock(DEVICE_COUNT)) {
+            val env: Environment<Instant> = gridWithTimeIntervalBetweenRounds()
+            val subsequentRounds = 2
+            env.runAllDevices(times = subsequentRounds)
+            val node = env.nodes.find { it.id == 0 }
+            // put device 0 at virtual time 20
+            // (it was already at virtual time 8, as the timestamp has been increased twice by SEQUENTIAL_FREQUENCY)
+            val driftTime = DEVICE_COUNT * 3
+            increaseTime(node!!.id, driftTime)
+            // execute a single round with the fast-forwarded device
+            env.runAllDevices(times = 1)
+            // simulate the death of device 0
+            env.nodes.filter { it.id != node.id }.forEach { n -> n.cycle() }
+            val deadDeviceTime = (DEVICE_COUNT * subsequentRounds) + driftTime
+            val inSynch = deadDeviceTime + DEVICE_COUNT
+            val expected = listOf(deadDeviceTime, inSynch, inSynch, inSynch)
+            env.nodes.forEachIndexed { index, node ->
+                env.shouldBeInstant(
+                    node.id,
+                    DISTANT_PAST + expected[index].seconds,
+                )
+            }
+        }
     }
 
     companion object {
--- collektive-stdlib/src/commonTest/kotlin/it/unibo/collektive/stdlib/test/TimeReplicatedTest.kt
+++ collektive-stdlib/src/commonTest/kotlin/it/unibo/collektive/stdlib/test/TimeReplicatedTest.kt
@@ -11,8 +11,11 @@ package it.unibo.collektive.stdlib.test
 import io.kotest.matchers.shouldBe
 import it.unibo.collektive.stdlib.processes.timeReplicated
 import it.unibo.collektive.stdlib.spreading.nonStabilizingGossip
+import it.unibo.collektive.stdlib.test.MultiClock.Companion.DEVICE_COUNT
+import it.unibo.collektive.stdlib.test.MultiClock.Companion.GRID_SIZE
 import it.unibo.collektive.testing.Environment
 import it.unibo.collektive.testing.mooreGrid
+import kotlinx.datetime.Instant
 import kotlin.test.BeforeTest
 import kotlin.test.Test
 import kotlin.time.Duration.Companion.seconds
@@ -21,45 +24,45 @@ import kotlin.time.ExperimentalTime
 @OptIn(ExperimentalTime::class)
 class TimeReplicatedTest {
 
-    @BeforeTest
-    fun setup() { // before each test
-        setupTimes()
-    }
-
     @Test
     fun `Time replicated gossip should converge after a few rounds`() {
-        val env: Environment<Int> = gridWithExecutionFrequency()
-        env.executeSubsequentRounds(times = NUM_DEVICES)
-        val finalStatus = env.status().values.distinct()
-        finalStatus.size shouldBe 1
-        finalStatus.first() shouldBe env.nodes.maxBy { it.id }.id // should be 3
+        with(MultiClock(DEVICE_COUNT)) {
+            val env: Environment<Int> = gridWithTimeIntervalBetweenRounds()
+            env.runAllDevices(times = DEVICE_COUNT)
+            val finalStatus = env.status().values.distinct()
+            finalStatus.size shouldBe 1
+            finalStatus.first() shouldBe env.nodes.maxBy { it.id }.id // should be 3
+        }
     }
 
     @Test
     fun `Time replicated gossip should stabilize after a few rounds from disruptive event`() {
-        val env: Environment<Int> = gridWithExecutionFrequency()
-        env.executeSubsequentRounds(times = NUM_DEVICES)
-        env.status().values.distinct()
-        env.removeNode(3) // simulate device 3 failure
-        env.executeSubsequentRounds(times = NUM_DEVICES * 2)
-        val finalStatus = env.status().values.distinct()
-        finalStatus.size shouldBe 1
-        finalStatus.first() shouldBe env.nodes.maxBy { it.id }.id // should be 2 now
+        with(MultiClock(DEVICE_COUNT)) {
+            val env: Environment<Int> = gridWithTimeIntervalBetweenRounds()
+            env.runAllDevices(times = DEVICE_COUNT)
+            env.status().values.distinct()
+            env.removeNode(3) // simulate device 3 failure
+            env.runAllDevices(times = DEVICE_COUNT * 2)
+            val finalStatus = env.status().values.distinct()
+            finalStatus.size shouldBe 1
+            finalStatus.first() shouldBe env.nodes.maxBy { it.id }.id // should be 2 now
+        }
     }
 
     companion object {
         /**
          * Creates a grid environment of the specified size where each node has a shared clock with execution frequency adjustment.
          */
-        private fun gridWithExecutionFrequency() = mooreGrid<Int>(GRID_SIZE, GRID_SIZE, { _, _ ->
+        context(clock: MultiClock)
+        private fun gridWithTimeIntervalBetweenRounds() = mooreGrid<Int>(GRID_SIZE, GRID_SIZE, { _, _ ->
             Int.MIN_VALUE
         }) {
             timeReplicated(
-                currentTime = times[localId],
+                currentTime = clock[localId],
                 maxReplicas = 4,
                 timeToSpawn = 3.seconds,
                 process = { nonStabilizingGossip(value = localId, reducer = ::maxOf) },
-            ).also { increaseTime(localId, SEQUENTIAL_FREQUENCY) }
+            ).also { clock.increaseTime(localId, DEVICE_COUNT) }
         }
     }
 }
