6228c7513a91842cc4215c1dbd88f758d0696155
Danilo Pianini <danilo.pianini@unibo.it>
2025-12-15 16:01:38 +0100

    chore: remove copyright block from build.gradle.kts

--- collektive-gradle-plugin/build.gradle.kts
+++ collektive-gradle-plugin/build.gradle.kts
@@ -8,14 +8,6 @@
 
 import com.github.gmazzo.buildconfig.BuildConfigTask
 
-/*
- * Copyright (c) 2023-2025, Danilo Pianini, Nicolas Farabegoli, Elisa Tronetti,
- * and all authors listed in the `build.gradle.kts` and the generated `pom.xml` file.
- *
- * This file is part of Collektive, and is distributed under the terms of the Apache License 2.0,
- * as described in the LICENSE file in this project's repository's top directory.
- */
-
 plugins {
     `java-gradle-plugin`
     alias(libs.plugins.build.config)
